01aa8e5b6e81539f1fd65f6c8b2c052effcb595d
Chad Quilter <chadquilter@hotmail.com>
2017-10-23 20:01:12 -0500

    add auth controller updates

--- app/User.php
+++ app/User.php
@@ -8,14 +8,33 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
-
+    protected $table = 'users';
+    public $primaryKey = 'id';
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
+    public $timestamps = true;
+
+    public $fillable = [
+        'name',
+        'email',
+        'password',
+        'user_employee',
+        'user_web_admin',
+        'user_quote_edit',
+        'user_quote_view',
+        'user_job_edit',
+        'user_job_view',
+        'user_invoice_edit',
+        'user_invoice_view',
+        'user_reciept_add',
+        'user_reciept_view',
+        'user_schedule_edit',
+        'user_schedule_view',
+        'user_created_by',
+        'user_modified_by',
     ];
 
     /**
@@ -26,4 +45,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function jobs(){
+        return $this->hasMany('App\Job');
+    }
+
+    public function quotes(){
+      return $this->hasMany('App\Quote');
+    }
 }
