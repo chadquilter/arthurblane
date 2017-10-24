1954ce82d5d87523daa3e3bdd76c486194dde5cb
Chad Quilter <chadquilter@hotmail.com>
2017-10-24 18:12:27 -0500

    add address section

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -104,7 +104,7 @@
                               <br> {{$address->address1}}
                               <br> {{$address->city}} , {{$address->state}}
                               <br> {{$address->zipcode}}
-                              <br>
+
                               <div class="mapouter">
                                 <div class="gmap_canvas">
                                   <iframe width="200" height="200" id="gmap_canvas" src="https://maps.google.com/maps?q=university of san francisco&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
