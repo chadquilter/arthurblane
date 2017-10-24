23936508025e728833425b3a5b2f818fb29807b3
Chad Quilter <chadquilter@hotmail.com>
2017-10-23 19:39:29 -0500

    commit email updates

--- config/mail.php
+++ config/mail.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'driver' => env('MAIL_DRIVER', 'smtp'),
+    'driver' => env('MAIL_DRIVER', 'sendmail'),
 
     /*
     |--------------------------------------------------------------------------
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.arthurblane.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -60,6 +60,10 @@ return [
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
+    'from' => [
+        'address' => env('MAIL_FROM_ADDRESS', '-fadmin@arthurblane.com'),
+        'name' => env('MAIL_FROM_NAME', 'Recieved Quote from Arthur Blane'),
+    ],
     /*
     |--------------------------------------------------------------------------
     | E-Mail Encryption Protocol
