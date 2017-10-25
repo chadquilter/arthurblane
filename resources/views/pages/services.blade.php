8685fa195135c40c5ce5c38cb9f9344edd05478a
Chad Quilter <chadquilter@hotmail.com>
2017-10-25 17:31:39 -0500

    update logo

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,25 +1,6 @@
 @extends('layouts.front')
 
 @section('content')
-	<div class="row">
-		<br>
-		<div class="col-md-6">
-			<div class="image_display_r">
-				<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_TILE2.JPG" style="max-height:450%; width: 100%; display: block;">
-				<div class="image_display_text_container">
-					<p>Our work is a cut above the rest!</p>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-6">
-			<div class="image_display_r">
-				<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_SHOWER1.JPG" style="max-height:450%; width: 100%; display: block;">
-				<div class="image_display_text_container">
-					<p>Our work is a cut above the rest!</p>
-				</div>
-			</div>
-		</div>
-	</div>
 	<br>
 	<div class="well">
 		<div class="service-text">
@@ -53,4 +34,25 @@
 		</div>
 		<br>
 	</div>
+	<div class="row">
+		<br>
+		<div class="col-md-6">
+			<div class="image_display_r">
+				<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_TILE2.JPG" style="max-height:450%; width: 100%; display: block;">
+				<div class="image_display_text_container">
+					<p>Our work is a cut above the rest!</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div class="image_display_r">
+				<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_SHOWER1.JPG" style="max-height:450%; width: 100%; display: block;">
+				<div class="image_display_text_container">
+					<p>Our work is a cut above the rest!</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<br>
+
 @endsection
