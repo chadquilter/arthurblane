f1c7b95fcf10b0066269ee45b720c225b0f7d3dc
Chad Quilter <chadquilter@hotmail.com>
2017-11-05 15:04:20 -0600

    add new item management

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -2,25 +2,58 @@
 
 @section('content')
   <br>
-  <div>
-    @if(count($items) > 0)
-        @foreach($items as $item)
-            <div class="well">
-                <h3><a href="/items/{{$item->id}}"> {{$item->item_name}} </a> </h3>
-                <br>&nbsp
-                <div id="web_space">
-                    {!!$item->item_summary!!}
-                    <center>
-                        &nbsp</br>
-                        <hr>
-                        <small>Item Listing: {{$item->created_at}} </small>
-                    </center>
+  <div class="container">
+      <div class="row">
+          <div class="col-md-8 col-md-offset-2">
+              <div class="panel panel-default">
+                  <div class="panel-heading">Manage Items</div>
+
+                  <div class="panel-body">
+                      @if (session('status'))
+                          <div class="alert alert-success">
+                              {{ session('status') }}
+                          </div>
+                      @endif
+
+                      @if(count($items) > 0)
+                      <table class="table table-striped table-hover table-sm table-responsive">
+                        <thead class="thead-inverse">
+                          <tr>
+                              <th nowrap><h3><span class="badge">{{ $jobs->total() }}</span> Item Entries:</h3></th>
+                              <th></th>
+                              <th></th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          @foreach($items as $item)
+                          <tr>
+                              <td width=80%><strong>Job: </strong>{{$item->item_name}}</td>
+                              <td><a href="/items/{{$item->id}}/edit" class="btn btn-default">Edit</a></td>
+                              <td>
+                                {!!Form::open(['action' => ['ItemsController@destroy', $item->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+                                    {{Form::hidden('_method', 'DELETE')}}
+                                    {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                                {!!Form::close()!!}
+                              </td>
+                          </tr>
+                          @endforeach
+                        </tbody>
+                        <tfoot>
+                          <tr>
+                            <td>
+                              {{$items->links()}}
+                            </td>
+                            <td></td>
+                            <td></td>
+                          </tr>
+                        </tfoot>
+                      </table>
+                      @else
+                        <p>No Items found</p>
+                      @endif
+                  </div>
                 </div>
             </div>
-        @endforeach
-        {{$items->links()}}
-    @else
-        <p>No Items found</p>
-    @endif
-  </div>
+        </div>
+    </div>      
 @endsection
