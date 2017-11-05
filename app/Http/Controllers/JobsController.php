00a588402305e8af19f27553a57165b22d67a014
Chad Quilter <chadquilter@hotmail.com>
2017-11-05 17:19:14 -0600

    add item dashboard badges

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -56,7 +56,7 @@ class JobsController extends Controller
             'job_invoiced' => 'job_invoiced:'
         );
 
-        $items = Item::where('item_status', 'like', '1')->get(['id', 'item_name']);
+        $items = Item::where('item_active', 'like', '1')->get(['id', 'item_name']);
 
         $bool_types = array(
             1 => 'Yes',
