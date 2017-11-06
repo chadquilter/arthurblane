564dc0fc9cdd7619194428c956d70bff9d2ea284
Chad Quilter <chadquilter@hotmail.com>
2017-11-05 22:15:21 -0600

    add info class

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -51,10 +51,12 @@
       @endif
     </div>
     @if (count($job_option_types) > 0)
-    <div class="alert alert-success" role="alert">
+
       <h3>Job Options:</h3>
       <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#job_option_group">Expand/Collapse Options</button>
+
       <div id="job_option_group" class="form=group collapse">
+        <div class="alert alert-success" role="alert">
         <br>
         @foreach ($job_option_types as $job_option_id => $job_option_name)
           @if($loop->first || $loop->iteration === 6)
