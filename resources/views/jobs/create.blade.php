e76eae4de6fa9c7a0d2411b7e9d5a95f75eca3a0
Chad Quilter <chadquilter@hotmail.com>
2017-11-05 23:39:03 -0600

    add custom js

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -122,7 +122,7 @@
 
         var myDiv = document.getElementById("item_div_"+i);
         //Create array of options to be added
-        var array = ["Volvo","Saab","Mercades","Audi"];
+        var array = items;
 
         //Create and append select list
         var selectList = document.createElement("select");
