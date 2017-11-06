d9906ccb69a44155d4bd9305405d9433b73570b2
Chad Quilter <chadquilter@hotmail.com>
2017-11-05 21:56:03 -0600

    nice laravel does loop iteration tracking....

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -51,10 +51,10 @@
       @endif
     </div>
     @if (count($job_option_types) > 0)
-    <h3>Job Options:</h3><br>
+    <h3>Job Options:</h3>
     <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#job_option_group">Expand/Collapse Options</button>
-    <br>
     <div id="job_option_group" class="form=group collapse">
+      <br>
       @foreach ($job_option_types as $job_option_id => $job_option_name)
         @if($loop->first || $loop->iteration === 6)
           <div class="row">
