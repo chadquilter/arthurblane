1f17a726025d82f4a9e0823908187d9b33bca635
Chad Quilter <chadquilter@gmail.com>
2017-11-07 07:05:07 -0600

    update js/scss asset files

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -2,8 +2,9 @@
 <html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'MDG') }}</title>
@@ -13,11 +14,19 @@
     <link href="/css/image_styles.css" rel="stylesheet">
 
     <script src="{{ asset('js/app.js') }}"></script>
+
+
 </head>
 <body>
     <div id="app">
         @include('inc.navbar')
-
+        <div class="body-container" style="border-color:#f6f6f6;border-bottom-width: 0px;border-top-width: 0px;border-bottom-style: solid;border-top-style: solid;padding-bottom:0px;padding-top:0px;padding-left:0px;padding-right:0px;background-attachment:fixed;background-color:#fff;background-position:center top;background-repeat:repeat-x;background-image: url(/images/art-bg.jpg); width=100%;">
+          <div class="container">
+            @include('inc.messages')
+            @yield('content')
+          </div>
+        </div>
+        @include('inc.footer')
     </div>
 
 </body>
