a28b89723446a8588db1bb4802d1493774d74bfe
Chad Quilter <chadquilter@hotmail.com>
2017-11-07 01:29:52 -0600

    add new models

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -41,7 +41,7 @@
                         {{!! $jobItem !!}}
                         <input name="item_amount[]" id="item_amount_{{ $loop->iteration }}" type="number" class="form-control" value="{{!! $jobItem !!}}" step="any" maxlength="10" size="10">
                         <strong>QTY: </strong>
-                        <input id="item_qty_{{$loop->iteration}}" name="item_qty[]" type="number" value="{{ $jobitem[2] }}" class="form-control" step="any" maxlength="10" size="10">
+                        <input id="item_qty_{{$loop->iteration}}" name="item_qty[]" type="number" value="" class="form-control" step="any" maxlength="10" size="10">
                         <input type="hidden" name="itemID[]" id="itemID{{$loop->iteration}}" value="{{$loop->iteration}}">
                       </td>
                       <td width="5%">
