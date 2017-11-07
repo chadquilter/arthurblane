b170f3094b50e4eeeb34c159400f67991d06610a
Chad Quilter <chadquilter@gmail.com>
2017-11-07 09:21:07 -0600

    update scss file

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -2,18 +2,21 @@
 <html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'MDG') }}</title>
     <!-- Styles -->
-
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="/css/footer.css" rel="stylesheet">
     <link href="/css/image_styles.css" rel="stylesheet">
 
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
     <script src="{{ asset('js/app.js') }}"></script>
-    <link href="/css/app.css" rel="stylesheet">
+
 
 </head>
 <body>
