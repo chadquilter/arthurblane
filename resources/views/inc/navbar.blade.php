28545e92767f71afc6a9b3364f75fd8630a5ad6a
Chad Quilter <chadquilter@gmail.com>
2017-11-07 06:55:07 -0600

    update js/scss asset files

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,56 +1,33 @@
-<header class="masthead">
-        
-        <!-- Branding Image -->
-        <a class="navbar-brand" href="{{ url('/') }}">
-           <img alt="{{ config('app.name', 'MDG') }}" src="/images/logo-brand.png">
-       </a>
-
-         <nav class="navbar navbar-expand-md navbar-light bg-light rounded mb-3">
-                 <div class="navbar-header">
-                     <!-- Collapsed Hamburger -->
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#app-navbar-collapse" aria-controls="app-navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
-                      <span class="navbar-toggler-icon"></span>
-                    </button>
-
-
-                 </div>
-                 <div class="collapse navbar-collapse" id="navbarCollapse">
-                     <!-- Left Side Of Navbar -->
-                     <ul class="navbar-nav text-md-center nav-justified w-100">
-                        <li class="nav-item">&nbsp</li>
-                        <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/quotes">Quote</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/services">Services</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/jobs">Jobs</a></li>
-                     </ul>
-
-                     <!-- Right Side Of Navbar -->
-                     <ul class="nav navbar-nav navbar-right">
-                         <!-- Authentication Links -->
-                         @guest
-                             <li><a href="{{ route('login') }}">Login</a></li>
-                         @else
-                             <li class="nav-item dropdown">
-                                 <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                     {{ Auth::user()->name }} <span class="caret"></span>
-                                 </a>
-
-                                 <ul class="dropdown-menu" role="menu">
-                                     <li><a href="/dashboard">Dashboard</a></li>
-                                    <li>
-                                        <a href="{{ url('/logout') }}"
-                                            onclick="event.preventDefault();
-                                            document.getElementById('logout-form').submit();">
-                                             Logout
-                                         </a>
-
-                                         <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                             {{ csrf_field() }}
-                                         </form>
-                                     </li>
-                                 </ul>
-                             </li>
-                         @endguest
-                     </ul>
-             </div>
-         </nav>
+<nav class="navbar navbar-expand-md navbar-light bg-light rounded mb-3">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav text-md-center nav-justified w-100">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Projects</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Services</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Downloads</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown01">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
