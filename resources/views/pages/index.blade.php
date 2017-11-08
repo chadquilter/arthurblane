3427dc1ec66564acde28a462fa13502aa803c781
Chad Quilter <chadquilter@hotmail.com>
2017-11-08 15:39:30 -0600

    add new bootstrap 4 refs

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -14,11 +14,12 @@
 
       <div class="jumbotron jumbotron-fluid">
         <center>
-          <h1>{{$title}}</h1>
+          <h1 class="display-3">{{$title}}</h1>
           <p class="lead">
             As a Texas custom home builder for over 10 years, we have made thousands of happy customers' dream home a reality by delivering on that promise.
             We would love to help you! For a free custom design meeting, contact us today. Let's get started on your new, custom dream home or new addition now!
           </p>
+          <hr class="my-4">
         </center>
       </div>
 
@@ -32,13 +33,14 @@
             </div>
           </div>
         </div>
-        <div class="col-md-4 jumbotron jumbotron-fluid">
+        <div class="col-md-4">
+          <div class="jumbotron jumbotron-fluid">
               <p class="lead">
                 {{ config('app.name', 'MDG') }} has helped thousands of happy homeowners across Texas build the new custom-designed home of their dreams.
                 We build "eco-friendly" green custom homes of all sizes for all budgets. Whatever custom home you have in mind, from a quaint cottage in Austin,
                 a rambling farm house in the Hill Country or an elegant luxury estate home in Houston, Dallas or San Antonio, we can build, repair, or add to it at an affordable price.
               </p>
-
+          </div>
         </div>
       </div>
     </div>
