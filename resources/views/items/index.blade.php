df4676c7ce268877de7ae83bc06ea4a6e742b9e4
Chad Quilter <chadquilter@gmail.com>
2017-11-08 05:50:05 -0600

    update dashboard

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,25 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-  <br>
   <div class="container">
-      <div class="row">
-          <div class="col-md-8 col-md-offset-2">
-              <div class="panel panel-default">
-                  <div class="panel-heading">Manage Items</div>
-                  <div class="panel-body">
-                      @if (session('status'))
-                          <div class="alert alert-success">
-                              {{ session('status') }}
-                          </div>
-                      @endif
-                      <a href="/dashboard" class="btn btn-primary">Back to Dashboard</a>
-                      <a href="/items/create" class="btn btn-success">Add Item</a>
+  <div class="card mx-auto">
+    <br>
+    <h1 class="card-title">Dashboard</h1>
+    <div class="card-block">
+      @if (session('status'))
+      <div class="alert alert-success">
+            {{ session('status') }}
+      </div>
+      @endif
+      <div>
+        <a href="/jobs/create" class="btn btn-primary">Create Job</a>
+        <a href="/quotes" class="btn btn-success">Create Quote</a>
+        <a href="/address" class="btn btn-warning">Manage Addresses</a>
+        <a href="/items" class="btn btn-warning">Manage Items</a>
+        <br>
+      </div>
+
                       @if(count($items) > 0)
-                      <table class="table table-striped table-hover table-sm table-responsive">
+                      <table class="table table-striped table-hover table-responsive">
                         <thead class="thead-inverse">
                           <tr>
-                              <th nowrap><h3><span class="badge">{{ $items->total() }}</span> Total Items:</h3></th>
+                              <th nowrap><h3><span class="badge badge-secondary">{{ $items->total() }}</span> Total Items:</h3></th>
                               <th></th>
                               <th></th>
                           </tr>
@@ -33,8 +37,8 @@
                                 <strong> Cost: </strpmg> <span class="badge"> {{$item->item_amount}} </span>
                                 <strong> QTY: </strong> <span class="badge"> {{$item->item_count}} </span>
                               </td>
-                              <td><a href="/items/{{$item->id}}/edit" class="btn btn-default">Edit</a></td>
-                              <td>
+                              <td width=10%><a href="/items/{{$item->id}}/edit" class="btn btn-default">Edit</a></td>
+                              <td width=10%>
                                 {!!Form::open(['action' => ['ItemsController@destroy', $item->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
                                     {{Form::hidden('_method', 'DELETE')}}
                                     {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
@@ -56,8 +60,7 @@
                       @else
                         <p>No Items found</p>
                       @endif
-                  </div>
-                </div>
+
             </div>
         </div>
     </div>
