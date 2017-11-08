860a39d18556df053f43802191ee76b75bb98525
Chad Quilter <chadquilter@gmail.com>
2017-11-08 05:11:55 -0600

    update dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,8 +19,9 @@
       <br>
     </div>
 
+    <center>
     @if(count($jobs) > 0)
-    <table class="table table-striped table-hover table-responsive mx-auto">
+    <table class="table table-striped table-hover table-responsive">
       <thead class="thead-inverse">
         <tr>
           <th nowrap><h3><span class="badge badge-secondary">{{ $jobs->total() }}</span> Job Entries:</h3></th>
@@ -87,51 +88,52 @@
     @endif
 
     @if(count($addresses) > 0)
-                    <table class="table table-striped table-hover table-responsive">
-                        <thead class="thead-inverse">
-                        <tr>
-                            <th nowrap><h3><span class="badge badge-secondary">{{ $addresses->total() }}</span> Saved Addresses:</h3></th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        @foreach($addresses as $address)
-                        <tr>
-                            <td width=80%><strong>Address: </strong>
-                              <div class=row>
-                                <div class="col-md-4">
-                                  <br> {{$address->address1}}
-                                  <br> {{$address->city}} , {{$address->state}}
-                                  <br> {{$address->zipcode}}
-                                </div>
-                                <div class="col-md-8">
-                                  <div class="mapouter">
-                                    <div class="gmap_canvas">
-                                      <iframe width="200" height="200" id="gmap_canvas" src="https://maps.google.com/maps?q={{$address->address1}},{{$address->city}},{{$address->state}},{{$address->zipcode}}=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-                                    </div>
-                                    <style>.mapouter{overflow:hidden;height:200px;width:200px;}.gmap_canvas {background:none!important;height:200px;width:200px;}</style>
-                                  </div>
-                              </div>
-                            </td>
-                            <td><a href="/address/{{$address->id}}/edit" class="btn btn-primary">Edit</a></td>
-                            <td>
-                              {!!Form::open(['action' => ['AddressController@destroy', $address->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
-                                  {{Form::hidden('_method', 'DELETE')}}
-                                  {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
-                              {!!Form::close()!!}
-                            </td>
-                        </tr>
-                        @endforeach
-                      </tbody>
-                      <tfoot>
-                        <tr>
-                          <td>{{$addresses->links()}}</td>
-                          <td></td>
-                          <td></td>
-                        </tr>
-                      </tfoot>
-                    </table>
+    <table class="table table-striped table-hover table-responsive">
+      <thead class="thead-inverse">
+        <tr>
+          <th nowrap><h3><span class="badge badge-secondary">{{ $addresses->total() }}</span> Saved Addresses:</h3></th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($addresses as $address)
+        <tr>
+          <td width=80%><strong>Address: </strong>
+            <div class=row>
+              <div class="col-md-4">
+                <br> {{$address->address1}}
+                <br> {{$address->city}} , {{$address->state}}
+                <br> {{$address->zipcode}}
+              </div>
+              <div class="col-md-8">
+                <div class="mapouter">
+                  <div class="gmap_canvas">
+                    <iframe width="200" height="200" id="gmap_canvas" src="https://maps.google.com/maps?q={{$address->address1}},{{$address->city}},{{$address->state}},{{$address->zipcode}}=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                  </div>
+                  <style>.mapouter{overflow:hidden;height:200px;width:200px;}.gmap_canvas {background:none!important;height:200px;width:200px;}</style>
+                </div>
+              </div>
+            </div>
+          </td>
+          <td><a href="/address/{{$address->id}}/edit" class="btn btn-primary">Edit</a></td>
+          <td>
+            {!!Form::open(['action' => ['AddressController@destroy', $address->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+            {{ Form::hidden('_method', 'DELETE') }}
+            {{ Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+            {!!Form::close()!!}
+          </td>
+        </tr>
+        @endforeach
+      </tbody>
+      <tfoot>
+        <tr>
+          <td>{{$addresses->links()}}</td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tfoot>
+    </table>
     @endif
   </div>
 </div>
