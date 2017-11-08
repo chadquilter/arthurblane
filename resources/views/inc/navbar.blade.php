08e4dba24aa908d84810a2a62704ab2aa87a990a
Chad Quilter <chadquilter@hotmail.com>
2017-11-07 19:30:37 -0600

    add new bootstrap 4 refs

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,49 +1,33 @@
-<header class="masthead">
 
-         <nav class="navbar navbar-expand-md navbar-light bg-light rounded mb-3">
-           <!-- Branding Image -->
-           <a class="navbar-brand" href="{{ url('/') }}">
-              <img alt="{{ config('app.name', 'MDG') }}" src="/images/logo-brand.png">
-          </a>
-           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-             <span class="navbar-toggler-icon"></span>
-           </button>
-           <div class="collapse navbar-collapse" id="navbarCollapse">
-             <ul class="navbar-nav text-md-center nav-justified w-100">
-                      <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
-                      <li class="nav-item"><a class="nav-link" href="/quotes">Quote</a></li>
-                      <li class="nav-item"><a class="nav-link" href="/services">Services</a></li>
-                      <li class="nav-item"><a class="nav-link" href="/jobs">Jobs</a></li>
-              </ul>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-                     <!-- Right Side Of Navbar -->
-                     <ul class="nav navbar-nav navbar-right">
-                         <!-- Authentication Links -->
-                         @guest
-                           <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
-                         @else
-                             <li class="nav-item dropdown">
-                                 <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                     {{ Auth::user()->name }}
-                                 </a>
-
-                                 <ul class="dropdown-menu" aria-labelledby="Dashboard">
-                                     <li><a class="dropdown-item" href="/dashboard">Dashboard</a></li>
-                                    <li>
-                                        <a class="dropdown-item" href="{{ url('/logout') }}"
-                                            onclick="event.preventDefault();
-                                            document.getElementById('logout-form').submit();">
-                                             Logout
-                                         </a>
-
-                                         <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                             {{ csrf_field() }}
-                                         </form>
-                                     </li>
-                                 </ul>
-                             </li>
-                         @endguest
-                     </ul>
-             </div>
-         </nav>
-    </header>
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
