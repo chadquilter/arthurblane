c8e23762fb1f242fcf70ec9c16a57386568dbf92
Chad Quilter <chadquilter@hotmail.com>
2017-11-08 17:58:39 -0600

    add new bootstrap 4 refs

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -32,25 +32,9 @@
           <span class="sr-only">Next</span>
         </a>
       </div>
-    </div>
-
-      <div class="image_display_r">
-        <img class="img-fluid" alt="A scenic home image" src="/images/dusk-home.jpg" style="max-height:100%; width: 100%; display: block;">
         <div class="image_display_text_container">
           <p>Our work is a cut above the rest!</p>
         </div>
-      </div>
-      <br>
-
-      <div class="jumbotron">
-        <center>
-          <h1 class="display-3">{{$title}}</h1>
-          <p class="lead">
-            As a Texas custom home builder for over 10 years, we have made thousands of happy customers' dream home a reality by delivering on that promise.
-            We would love to help you! For a free custom design meeting, contact us today. Let's get started on your new, custom dream home or new addition now!
-          </p>
-          <hr class="my-4">
-        </center>
       </div>
 
       <br>
