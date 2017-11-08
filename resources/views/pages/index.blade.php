1a37aeef4a4b49d6fcb4b694ef1f67a04dbb75ae
Chad Quilter <chadquilter@hotmail.com>
2017-11-08 17:57:27 -0600

    add new bootstrap 4 refs

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,6 +4,36 @@
 
     <div class="container">
       <br>
+
+      <div class="image_display_r">
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="/images/dusk-home.jpg" alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="/images/dusk-home.jpg" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="/images/dusk-home.jpg" alt="Third slide">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+
       <div class="image_display_r">
         <img class="img-fluid" alt="A scenic home image" src="/images/dusk-home.jpg" style="max-height:100%; width: 100%; display: block;">
         <div class="image_display_text_container">
