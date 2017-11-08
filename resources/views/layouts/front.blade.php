4ad5a799f900ca24804603ed3ceb229626d4f0f4
Chad Quilter <chadquilter@hotmail.com>
2017-11-08 00:04:15 -0600

    add new bootstrap 4 refs

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -3,22 +3,21 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'MDG') }}</title>
     <!-- Styles -->
-    <link href= {{ asset('/css/app.css') }} rel="stylesheet">
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
     <script src="{{ asset('js/app.js') }}"></script>
+     <link href="https://getbootstrap.com/docs/4.0/examples/starter-template/starter-template.css" rel="stylesheet">
 
 </head>
 <body>
     @include('inc.navbar')
-    <div id="app">
 
-    </div>
 
 </body>
     <script src="https://getbootstrap.com/dist/js/bootstrap.min.js"></script>
