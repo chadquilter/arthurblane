00db38729a0a6c31ff487ff88a3c7c23ac58442e
Chad Quilter <chadquilter@hotmail.com>
2017-11-08 02:44:45 -0600

    add new bootstrap 4 refs

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,97 +1,88 @@
 @extends('layouts.app')
 
 @section('content')
-<br>
+<div class="card">
+  <h1 class="card-title">Dashboard</h1>
+  <div class="card-block">
+    @if (session('status'))
+    <div class="alert alert-success">
+          {{ session('status') }}
+    </div>
+    @endif
+    <a href="/jobs/create" class="btn btn-primary">Create Job</a>
+    <a href="/quotes" class="btn btn-success">Create Quote</a>
+    <a href="/address" class="btn btn-warning">Manage Addresses</a>
+    <a href="/items" class="btn btn-warning">Manage Items</a>
+    <br>
 
-  <div class="card">
-    <div class="card-block">
-      <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+    @if(count($jobs) > 0)
+    <table class="table table-striped table-hover table-sm table-responsive">
+      <thead class="thead-inverse">
+        <tr>
+          <th nowrap><h3><span class="badge">{{ $jobs->total() }}</span> Job Entries:</h3></th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($jobs as $job)
+          <tr>
+            <td width=80%><strong>Job: </strong>{{$job->job_title}}</td>
+            <td><a href="/jobs/{{$job->job_id}}/edit" class="btn btn-default">Edit</a></td>
+            <td>
+              {!!Form::open(['action' => ['JobsController@destroy', $job->job_id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+                {{Form::hidden('_method', 'DELETE')}}
+                {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+              {!!Form::close()!!}
+            </td>
+          </tr>
+        @endforeach
+      </tbody>
+      <tfoot>
+      <tr>
+        <td>
+          {{$jobs->links()}}
+        </td>
+        <td></td>
+        <td></td>
+      </tr>
+      </tfoot>
+      </table>
+      @endif
+      @if(count($quotes) > 0)
+        <table class="table table-striped table-hover table-sm table-responsive">
+        <thead class="thead-inverse">
+          <tr>
+            <th nowrap><h3><span class="badge">{{ $quotes->total() }}</span> Quote Entries:</h3></th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+        @foreach($quotes as $quote)
+        <tr>
+          <td width=80%><strong>Quote: </strong> {{$quote->title}}</td>
+          <td><a href="/quotes/{{$quote->id}}/edit" class="btn btn-default">Edit</a></td>
+          <td>
+            {!!Form::open(['action' => ['QuotesController@destroy', $quote->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+            {{Form::hidden('_method', 'DELETE')}}
+            {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+            {!!Form::close()!!}
+          </td>
+        </tr>
+        @endforeach
+      </tbody>
+      <tfoot>
+        <tr>
+          <td>{{$quotes->links()}}</td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tfoot>
+    </table>
+    @endif
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <a href="/jobs/create" class="btn btn-primary">Create Job</a>
-                    <a href="/quotes" class="btn btn-success">Create Quote</a>
-                    <a href="/address" class="btn btn-warning">Manage Addresses</a>
-                    <a href="/items" class="btn btn-warning">Manage Items</a>
-                    <br>
-
-                    @if(count($jobs) > 0)
-                    <table class="table table-striped table-hover table-sm table-responsive">
-                      <thead class="thead-inverse">
-                        <tr>
-                            <th nowrap><h3><span class="badge">{{ $jobs->total() }}</span> Job Entries:</h3></th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        @foreach($jobs as $job)
-                        <tr>
-                            <td width=80%><strong>Job: </strong>{{$job->job_title}}</td>
-                            <td><a href="/jobs/{{$job->job_id}}/edit" class="btn btn-default">Edit</a></td>
-                            <td>
-                              {!!Form::open(['action' => ['JobsController@destroy', $job->job_id], 'method' => 'POST', 'class' => 'pull-right'])!!}
-                                  {{Form::hidden('_method', 'DELETE')}}
-                                  {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
-                              {!!Form::close()!!}
-                            </td>
-                        </tr>
-                        @endforeach
-                      </tbody>
-                      <tfoot>
-                        <tr>
-                          <td>
-                            {{$jobs->links()}}
-                          </td>
-                          <td></td>
-                          <td></td>
-                        </tr>
-                      </tfoot>
-                    </table>
-                    @endif
-
-                    @if(count($quotes) > 0)
-                    <table class="table table-striped table-hover table-sm table-responsive">
-                        <thead class="thead-inverse">
-                        <tr>
-                            <th nowrap><h3><span class="badge">{{ $quotes->total() }}</span> Quote Entries:</h3></th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        @foreach($quotes as $quote)
-                        <tr>
-                            <td width=80%><strong>Quote: </strong> {{$quote->title}}</td>
-                            <td><a href="/quotes/{{$quote->id}}/edit" class="btn btn-default">Edit</a></td>
-                            <td>
-                              {!!Form::open(['action' => ['QuotesController@destroy', $quote->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
-                                  {{Form::hidden('_method', 'DELETE')}}
-                                  {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
-                              {!!Form::close()!!}
-                            </td>
-                        </tr>
-                        @endforeach
-                      </tbody>
-                      <tfoot>
-                        <tr>
-                          <td>{{$quotes->links()}}</td>
-                          <td></td>
-                          <td></td>
-                        </tr>
-                      </tfoot>
-                    </table>
-                    @endif
-
-                    @if(count($addresses) > 0)
+    @if(count($addresses) > 0)
                     <table class="table table-striped table-hover table-sm table-responsive">
                         <thead class="thead-inverse">
                         <tr>
@@ -137,12 +128,7 @@
                         </tr>
                       </tfoot>
                     </table>
-                    @endif
-                  </div>
-              </div>
-          </div>
-      </div>
-
-    </div>
+    @endif
   </div>
+</div>
 @endsection
