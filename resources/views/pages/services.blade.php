a7bebba2946c9a3996de4c0a338882fd7f6c1dba
Chad Quilter <chadquilter@hotmail.com>
2017-11-08 16:54:33 -0600

    add new bootstrap 4 refs

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -10,23 +10,35 @@
 			</div>
 		</div>
 		<br>
-		<div class="jumbotron">
-			<center>
-				<h1 class="display-3">{{$title}}</h1>
-				<p class="lead">
-					@if(count($services) > 0)
-						<ul>
-							@foreach($services as $service)
-								<li>{{$service}}</li>
-							@endforeach
-						</ul>
-					@else
-						<li>Our services listing are currently under construction!</li>
-					@endif
-				</p>
-				<hr class="my-4">
-			</center>
+
+		<div class="row">
+			<div class="col-md-6">
+				<div class="jumbotron">
+					<h1 class="display-3">{{$title}}</h1>
+					<p class="lead">
+						@if(count($services) > 0)
+							<ul>
+								@foreach($services as $service)
+									<li>{{$service}}</li>
+								@endforeach
+							</ul>
+						@else
+							<li>Our services listing are currently under construction!</li>
+						@endif
+					</p>
+					<hr class="my-4">
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="image_display_r">
+					<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_BATHROOM1.JPG" style="max-height:100%; width: 100%; display: block;">
+					<div class="image_display_text_container">
+						<p>Our work is a cut above the rest!</p>
+					</div>
+				</div>
+			</div>
 		</div>
+
 		<br>
 		<div class="row">
 			<div class="col-md-6">
