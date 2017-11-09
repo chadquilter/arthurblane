5b1db34085132ef3bcfe40ec61b8c503c4567982
Chad Quilter <chadquilter@hotmail.com>
2017-11-08 19:01:59 -0600

    add new bootstrap 4 refs

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -11,14 +11,11 @@
           <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
         </ol>
         <div class="carousel-inner">
           <div class="carousel-item active">
             <img class="d-block w-100" src="/images/dusk-home.jpg" alt="First slide">
-            <div class="carousel-caption d-none d-md-block">
-              <h3> Things...</h3>
-              <p> Things...</p>
-            </div>
           </div>
           <div class="carousel-item">
             <img class="d-block w-100" src="/images/LSIDEA.JPG" alt="Second slide">
