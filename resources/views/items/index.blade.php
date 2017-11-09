553058749e0b649e6a12a4d94a3cc8256fb86e98
Chad Quilter <chadquilter@hotmail.com>
2017-11-08 22:48:07 -0600

    add new bootstrap 4 refs

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -34,8 +34,8 @@
                           @foreach($items as $item)
                           <tr>
                               <td width=80%>
-                                <strong> {{$item->item_name}} </strong>
-                                 Cost: </strpmg> <span class="badge badge-secondary"> {{$item->item_amount}} </span> 
+                                <h6> {{$item->item_name}} </h6>
+                                 Cost: </strpmg> <span class="badge badge-secondary"> {{$item->item_amount}} </span>
                                  QTY: </strong> <span class="badge badge-secondary"> {{$item->item_count}} </span>
                               </td>
                               <td width=10%><a href="/items/{{$item->id}}/edit" class="btn btn-primary">Edit</a></td>
