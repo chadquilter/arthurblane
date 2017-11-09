b6595a5b4ad5f2c56264e630718c711a96a4b3bd
Chad Quilter <chadquilter@hotmail.com>
2017-11-08 23:01:43 -0600

    add new bootstrap 4 refs

--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -1,94 +1,107 @@
 @extends('layouts.app')
 
 @section('content')
-  <br>
-  <div class="well">
-    <a href="/items" class="btn btn-primary">Back to Item Manager</a>
-    <br>
-    <h1>Add New Item</h1>
-    {!! Form::open(['action' => 'ItemsController@store', 'method' => 'POST', 'enctype' => 'multipart/data']) !!}
-    <div class="form=group">
-        {{Form::label('item_name', 'Item Name:')}}
-        {{Form::text('item_name',  '', ['class' => 'form-control', 'placeholder' => 'brick, 2x4 plywood, etc..'])}}
-    </div>
-    <div class="form=group">
-      {{Form::label('item_summary', 'Description:')}}
-      {{Form::textarea('item_summary', '', ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Item Description'])}}
-    </div>
-    <div class="row">
-    @if (count($item_types) > 0)
-      <div class="form=group col-xs-3">
-        {{Form::label('item_type', 'Item Type:')}}
-        @if(count($item_types) > 0)
-          {{ Form::select('item_type', $item_types, '', ['class' => 'form-control m-bot15']) }}
-        @else
-          <h1>No Types listed! Required field, call for help!:</h1>
-        @endif
-      </div>
-      @endif
-      <div class="form=group col-xs-3">
-        {{Form::label('item_weight', 'Item Weight:')}}
-        {{Form::number('item_weight',  '', ['class' => 'form-control', 'placeholder' => '0.00', 'step' => 'any'])}}
-      </div>
-      <div class="form=group col-xs-3">
-        {{Form::label('item_amount', 'Item Cost:')}}
-        {{Form::number('item_amount',  '', ['class' => 'form-control', 'placeholder' => '0.00', 'step' => 'any'])}}
-      </div>
-    </div>
-    <div class="row">
-      <div class="form=group col-xs-3">
-        {{Form::label('item_count', 'Number of Items:')}}
-        {{Form::number('item_count',  '', ['class' => 'form-control', 'placeholder' => '0.00', 'step' => 'any'])}}
-      </div>
-      @if (count($uom) > 0)
-        <div class="form=group col-xs-3">
-          {{Form::label('item_uom', 'Item Unit of Measure:')}}
-          @if(count($uom) > 0)
-            {{ Form::select('item_uom', $uom, '', ['class' => 'form-control m-bot15']) }}
-          @else
-            <h1>No Types listed! Required field, call for help!:</h1>
-          @endif
+  <div class="container">
+    <div class="jumbotron">
+      <h1 class="display-3">Manage Items</h1>
+      <div class="card mx-auto">
+        <br>
+        <div class="card-block">
+          <div>
+            <a href="/dashboard" class="btn btn-primary"> <<< Back to Dashboard</a>
+            <a href="/jobs/create" class="btn btn-primary">Create Job</a>
+            <a href="/quotes" class="btn btn-success">Create Quote</a>
+            <a href="/address" class="btn btn-warning">Manage Addresses</a>
+            <a href="/items/create" class="btn btn-warning">Create Item</a>
+          </div>
+          <br>
+          {!! Form::open(['action' => 'ItemsController@store', 'method' => 'POST', 'enctype' => 'multipart/data']) !!}
+          <div class="form=group">
+            {{Form::label('item_name', 'Item Name:')}}
+            {{Form::text('item_name',  '', ['class' => 'form-control', 'placeholder' => 'brick, 2x4 plywood, etc..'])}}
+          </div>
+          <div class="form=group">
+            {{Form::label('item_summary', 'Description:')}}
+            {{Form::textarea('item_summary', '', ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Item Description'])}}
+          </div>
+          <div class="row">
+            @if (count($item_types) > 0)
+              <div class="form=group col-md-6">
+                {{Form::label('item_type', 'Item Type:')}}
+                @if(count($item_types) > 0)
+                  {{ Form::select('item_type', $item_types, '', ['class' => 'form-control m-bot15']) }}
+                @else
+                  <h1>No Types listed! Required field, call for help!:</h1>
+                @endif
+              </div>
+            @endif
+            <div class="form=group col-md-6">
+              {{Form::label('item_weight', 'Item Weight:')}}
+              {{Form::number('item_weight',  '', ['class' => 'form-control', 'placeholder' => '0.00', 'step' => 'any'])}}
+            </div>
+            <div class="form=group col-md-6">
+              {{Form::label('item_amount', 'Item Cost:')}}
+              {{Form::number('item_amount',  '', ['class' => 'form-control', 'placeholder' => '0.00', 'step' => 'any'])}}
+            </div>
+          </div>
+          <div class="row">
+            <div class="form=group col-md-6">
+              {{Form::label('item_count', 'Number of Items:')}}
+              {{Form::number('item_count',  '', ['class' => 'form-control', 'placeholder' => '0.00', 'step' => 'any'])}}
+            </div>
+            @if (count($uom) > 0)
+              <div class="form=group col-md-6">
+                {{Form::label('item_uom', 'Item Unit of Measure:')}}
+                @if(count($uom) > 0)
+                  {{ Form::select('item_uom', $uom, '', ['class' => 'form-control m-bot15']) }}
+                @else
+                  <h1>No Types listed! Required field, call for help!:</h1>
+                @endif
+              </div>
+            @endif
+          </div>
+          <h3> Item Options </h3>
+          <button type="button" class="btn btn-primary" data-toggle="collapse" data-target="#job_option_group">Expand/Collapse Options</button>
+          <br> &nbsp
+          <div id="job_option_group" class="form=group collapse">
+            <div class="form=group">
+              {{Form::label('item_online_price', 'Show Online Prices? ')}}
+              {{Form::checkbox('item_online_price','1', false, ['class' => 'form-control'])}}
+            </div>
+            <div class="form=group">
+              {{Form::label('item_has_subitems', 'Has Subitems? ')}}
+              {{Form::checkbox('item_has_subitems','1', false, ['class' => 'form-control'])}}
+            </div>
+            <div class="form=group">
+              {{Form::label('item_is_oversized', 'Item is Oversized/Needs Cargo area? ')}}
+              {{Form::checkbox('item_is_oversized','1', false, ['class' => 'form-control'])}}
+            </div>
+            <div class="form=group">
+              {{Form::label('item_has_image', 'Item Stored Image? ')}}
+              {{Form::checkbox('item_has_image','1', false, ['class' => 'form-control'])}}
+            </div>
+            <div class="form=group">
+              {{Form::label('item_has_address', 'Item haas Address? ')}}
+              {{Form::checkbox('item_has_address','1', false, ['class' => 'form-control'])}}
+            </div>
+            <div class="form=group">
+              {{Form::label('item_active', 'Item is Active? ')}}
+              {{Form::checkbox('item_active','1', true, ['class' => 'form-control'])}}
+            </div>
+            <br>
+          </div>
+          <hr class="my-1">
+          <div class="page-buttons">
+            {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
+          </div>
+          {!! Form::close() !!}
+          <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+          <script>
+          CKEDITOR.replace( 'article-ckeditor' );
+          </script>
         </div>
-      @endif
+        <br>
+      </div>
     </div>
-    <h3> Item Options </h3>
-    <button type="button" class="btn btn-primary" data-toggle="collapse" data-target="#job_option_group">Expand/Collapse Options</button>
-    <br> &nbsp
-    <div id="job_option_group" class="form=group collapse">
-      <div class="form=group">
-        {{Form::label('item_online_price', 'Show Online Prices? ')}}
-        {{Form::checkbox('item_online_price','1', false, ['class' => 'form-control'])}}
-      </div>
-      <div class="form=group">
-        {{Form::label('item_has_subitems', 'Has Subitems? ')}}
-        {{Form::checkbox('item_has_subitems','1', false, ['class' => 'form-control'])}}
-      </div>
-      <div class="form=group">
-        {{Form::label('item_is_oversized', 'Item is Oversized/Needs Cargo area? ')}}
-        {{Form::checkbox('item_is_oversized','1', false, ['class' => 'form-control'])}}
-      </div>
-      <div class="form=group">
-        {{Form::label('item_has_image', 'Item Stored Image? ')}}
-        {{Form::checkbox('item_has_image','1', false, ['class' => 'form-control'])}}
-      </div>
-      <div class="form=group">
-        {{Form::label('item_has_address', 'Item haas Address? ')}}
-        {{Form::checkbox('item_has_address','1', false, ['class' => 'form-control'])}}
-      </div>
-      <div class="form=group">
-        {{Form::label('item_active', 'Item is Active? ')}}
-        {{Form::checkbox('item_active','1', true, ['class' => 'form-control'])}}
-      </div>
-      <br>
-    </div>
-    <div class="page-buttons">
-      {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
-    </div>
-    {!! Form::close() !!}
-    <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
-    <script>
-        CKEDITOR.replace( 'article-ckeditor' );
-    </script>
   </div>
-  <br>
 @endsection
--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -2,68 +2,66 @@
 
 @section('content')
   <div class="container">
-  <div class="jumbotron">
-  <h1 class="display-3">Manage Items</h1>
-  <div class="card mx-auto">
-    <br>
-    <div class="card-block">
-      @if (session('status'))
-      <div class="alert alert-success">
-            {{ session('status') }}
-      </div>
-      @endif
-      <div>
-        <a href="/dashboard" class="btn btn-primary"> <<< Back to Dashboard</a>
-        <a href="/jobs/create" class="btn btn-primary">Create Job</a>
-        <a href="/quotes" class="btn btn-success">Create Quote</a>
-        <a href="/address" class="btn btn-warning">Manage Addresses</a>
-        <a href="/items/create" class="btn btn-warning">Create Item</a>
+    <div class="jumbotron">
+      <h1 class="display-3">Manage Items</h1>
+      <div class="card mx-auto">
         <br>
-      </div>
-
-                      @if(count($items) > 0)
-                      <table class="table table-striped table-hover table-responsive">
-                        <thead class="thead-inverse">
-                          <tr>
-                              <th nowrap><h3><span class="badge badge-secondary">{{ $items->total() }}</span> Total Items:</h3></th>
-                              <th></th>
-                              <th></th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          @foreach($items as $item)
-                          <tr>
-                              <td width=80%>
-                                <strong> {{$item->item_name}} </strong>
-                                <hr class="my-1">
-                                Cost: </strpmg> <span class="badge badge-secondary"> {{$item->item_amount}} </span>
-                                QTY: </strong> <span class="badge badge-secondary"> {{$item->item_count}} </span>
-                              </td>
-                              <td width=10%><a href="/items/{{$item->id}}/edit" class="btn btn-primary">Edit</a></td>
-                              <td width=10%>
-                                {!!Form::open(['action' => ['ItemsController@destroy', $item->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
-                                    {{Form::hidden('_method', 'DELETE')}}
-                                    {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
-                                {!!Form::close()!!}
-                              </td>
-                          </tr>
-                          @endforeach
-                        </tbody>
-                        <tfoot>
-                          <tr>
-                            <td>
-                              {{$items->links()}}
-                            </td>
-                            <td></td>
-                            <td></td>
-                          </tr>
-                        </tfoot>
-                      </table>
-                      @else
-                        <p>No Items found</p>
-                      @endif
-
+        <div class="card-block">
+          @if (session('status'))
+            <div class="alert alert-success">
+              {{ session('status') }}
             </div>
+          @endif
+          <div>
+            <a href="/dashboard" class="btn btn-primary"> <<< Back to Dashboard</a>
+            <a href="/jobs/create" class="btn btn-primary">Create Job</a>
+            <a href="/quotes" class="btn btn-success">Create Quote</a>
+            <a href="/address" class="btn btn-warning">Manage Addresses</a>
+            <a href="/items/create" class="btn btn-warning">Create Item</a>
+            <br>
+          </div>
+          @if(count($items) > 0)
+            <table class="table table-striped table-hover table-responsive">
+              <thead class="thead-inverse">
+                <tr>
+                  <th nowrap><h3><span class="badge badge-secondary">{{ $items->total() }}</span> Total Items:</h3></th>
+                  <th></th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach($items as $item)
+                  <tr>
+                    <td width=80%>
+                      <strong> {{$item->item_name}} </strong>
+                      <hr class="my-1">
+                      Cost: </strpmg> <span class="badge badge-secondary"> {{$item->item_amount}} </span>
+                      QTY: </strong> <span class="badge badge-secondary"> {{$item->item_count}} </span>
+                    </td>
+                    <td width=10%><a href="/items/{{$item->id}}/edit" class="btn btn-primary">Edit</a></td>
+                    <td width=10%>
+                      {!!Form::open(['action' => ['ItemsController@destroy', $item->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+                      {{Form::hidden('_method', 'DELETE')}}
+                      {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                      {!!Form::close()!!}
+                    </td>
+                  </tr>
+                @endforeach
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td>
+                    {{$items->links()}}
+                  </td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </tfoot>
+            </table>
+          @else
+            <p>No Items found</p>
+          @endif
         </div>
+      </div>
     </div>
 @endsection
