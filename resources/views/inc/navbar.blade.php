2ae4417bcd441f13af63c3b5b73d9c5b319cb27c
Chad Quilter <chadquilter@hotmail.com>
2017-11-08 20:47:25 -0600

    add new bootstrap 4 refs

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -8,7 +8,7 @@
   <div class="collapse navbar-collapse" id="navbarsExampleDefault">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item {{ ( \Request::route()->getName() == 'index') ? 'active' : '' }}">
-        <a class="nav-link" href="/">Home {{ \Request::route()->getName() $request->path() }}</a>
+        <a class="nav-link" href="/">Home {{ \Request::url()  }}</a>
       </li>
       <li class="nav-item {{ ( Request::route()->getName() == 'quotes.index') ? 'active' : '' }}">
         <a class="nav-link" href="/quotes">Quote</a>
