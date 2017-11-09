89efaf03910a1f44c03e38f1eca48ee7607535da
Chad Quilter <chadquilter@hotmail.com>
2017-11-08 21:56:00 -0600

    add new bootstrap 4 refs

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -13,10 +13,11 @@
       </div>
       @endif
       <div>
-        <a href="/dashboard" class="btn btn-primary">Dashboard</a>
+        <a href="/dashboard" class="btn btn-primary">Back to Dashboard</a>
         <a href="/jobs/create" class="btn btn-primary">Create Job</a>
         <a href="/quotes" class="btn btn-success">Create Quote</a>
         <a href="/address" class="btn btn-warning">Manage Addresses</a>
+        <a href="/address" class="btn btn-warning">Create Item</a>
         <br>
       </div>
 
