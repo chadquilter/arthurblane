4e81f8924d146cc68cf0517da703d1206854c45b
Chad Quilter <chadquilter@hotmail.com>
2017-11-09 01:55:29 -0600

    add new bootstrap 4 refs

--- resources/views/address/index.blade.php
+++ resources/views/address/index.blade.php
@@ -28,7 +28,7 @@
           @endif
 
           @if(count($addresses) > 0)
-            <table class="table table-striped table-hover table-sm table-responsive border-secondary">
+            <table class="table table-striped table-hover table-sm table-responsive border border-secondary">
               <thead class="thead-inverse">
                 <tr>
                   <th nowrap><h3><span class="badge">{{ $addresses->total() }}</span> Saved Addresses:</h3></th>
