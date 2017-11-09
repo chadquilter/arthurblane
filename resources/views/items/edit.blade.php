342d7f93401aecf3664e5a9f30daefee99730937
Chad Quilter <chadquilter@hotmail.com>
2017-11-08 23:59:39 -0600

    add new bootstrap 4 refs

--- resources/views/items/edit.blade.php
+++ resources/views/items/edit.blade.php
@@ -11,7 +11,8 @@
             <a href="/items" class="btn btn-primary"> <<< Back to Manage Items </a>
             <a href="/dashboard" class="btn btn-primary"> <<< Back to Dashboard</a>
           <div>
-          <div class="btn-group btn-group" role="group" aria-label="links"
+            <br>
+          <div class="btn-group btn-group" role="group" aria-label="links">
             <a href="/jobs/create" class="btn btn-primary">Create Job</a>
             <a href="/quotes" class="btn btn-success">Create Quote</a>
             <a href="/address" class="btn btn-warning">Manage Addresses</a>
