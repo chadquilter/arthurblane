0ad9c56d8dca3fef9b26aa91b5c647e1b3367512
Chad Quilter <chadquilter@hotmail.com>
2017-11-10 17:27:21 -0600

    update dahsboard look

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
   <div class="container">
-    <div class="jumbotron border border-secondary">
+    <div class="jumbotron">
       <h1 class="display-3">Manage Items</h1>
-      <div class="card mx-auto border border-secondary">
+      <div class="card mx-auto">
         <br>
         <div class="card-body">
           <div class="btn-group" role="group" aria-label="links">
