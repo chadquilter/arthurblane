d45678d2385309c11b4fbe63aa53a1306c4b6e97
Chad Quilter <chadquilter@hotmail.com>
2017-11-10 17:53:41 -0600

    update dahsboard look

--- resources/views/quotes/create.blade.php
+++ resources/views/quotes/create.blade.php
@@ -3,70 +3,73 @@
 
 @section('content')
   <div class="container">
-  	<div class="row">
-  		<br>
-  		<div class="col-md-6">
-  			<div class="image_display_r">
-  				<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_BATHROOM2.JPG" style="max-height:100%; width: 100%; display: block;">
-  				<div class="image_display_text_container">
-  					<p>Our work is a cut above the rest!</p>
-  				</div>
-  			</div>
-  		</div>
-  		<div class="col-md-6">
-  			<div class="image_display_r">
-  				<img class="img-fluid" alt="A scenic home image" src="/images/CAB1.JPG" style="max-height:100%; width: 100%; display: block;">
-  				<div class="image_display_text_container">
-  					<p>Our work is a cut above the rest!</p>
-  				</div>
-  			</div>
-  		</div>
-  	</div>
-  	<br>
+    <div class="row">
+      <br>
+      <div class="col-md-6">
+        <div class="image_display_r">
+          <img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_BATHROOM2.JPG" style="max-height:100%; width: 100%; display: block;">
+          <div class="image_display_text_container">
+            <p>Our work is a cut above the rest!</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="image_display_r">
+          <img class="img-fluid" alt="A scenic home image" src="/images/CAB1.JPG" style="max-height:100%; width: 100%; display: block;">
+          <div class="image_display_text_container">
+            <p>Our work is a cut above the rest!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br>
     <div class="jumbotron">
       <div class="container">
         <h1 class="display-3">Create a Quote:</h1>
-        <br>
-        {!! Form::open(['action' => 'QuotesController@store', 'method' => 'POST', 'enctype' => 'multipart/data']) !!}
-        <div class="form=group">
-          {{Form::label('title', 'Name:')}}
-          {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Name'])}}
+        <div class="card mx-auto border border-secondary">
+          <div class="card-body">
+            {!! Form::open(['action' => 'QuotesController@store', 'method' => 'POST', 'enctype' => 'multipart/data']) !!}
+            <div class="form=group">
+              {{Form::label('title', 'Name:')}}
+              {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Name'])}}
+            </div>
+            <div id="app-6" class="form=group">
+              {{Form::label('phone', 'Phone:')}}
+              {{Form::text('phone', '', ['class' => 'form-control', 'placeholder' => 'Phone'])}}
+            </div>
+            <div class="form=group">
+              {{Form::label('email', 'Email:')}}
+              {{Form::text('email', '', ['class' => 'form-control', 'placeholder' => 'Email'])}}
+            </div>
+            <div class="form=group">
+              {{Form::label('description', 'Description:')}}
+              {{Form::textarea('description', '', ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Quote Description'])}}
+            </div>
+            <br>
+            <div class="form=group">
+              {{Form::label('notes', 'Notes:')}}
+              {{Form::text('notes', '', ['class' => 'form-control', 'placeholder' => 'Additional Notes'])}}
+            </div>
+            <br>
+            <div class="form=group">
+              {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
+            </div>
+            {!! Form::close() !!}
+            <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+            <script>
+            CKEDITOR.replace( 'article-ckeditor' );
+            </script>
+          </div>
         </div>
-        <div id="app-6" class="form=group">
-          {{Form::label('phone', 'Phone:')}}
-          {{Form::text('phone', '', ['class' => 'form-control', 'placeholder' => 'Phone'])}}
-        </div>
-        <div class="form=group">
-          {{Form::label('email', 'Email:')}}
-          {{Form::text('email', '', ['class' => 'form-control', 'placeholder' => 'Email'])}}
-        </div>
-        <div class="form=group">
-          {{Form::label('description', 'Description:')}}
-          {{Form::textarea('description', '', ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Quote Description'])}}
-        </div>
-        <br>
-        <div class="form=group">
-          {{Form::label('notes', 'Notes:')}}
-          {{Form::text('notes', '', ['class' => 'form-control', 'placeholder' => 'Additional Notes'])}}
-        </div>
-        <br>
-        <div class="form=group">
-          {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
-        </div>
-        {!! Form::close() !!}
-  		<script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
-  		<script>
-  				CKEDITOR.replace( 'article-ckeditor' );
-  		</script>
       </div>
-  	</div>
-  	<br>
-  	<div class="image_display_r">
-  		<img class="img-fluid"  alt="A scenic home image" src="/images/LSIDEA.JPG" style="max-height:100%; width: 100%; display: block;">
-  		<div class="image_display_text_container">
-  			<p>Custom Design!</p>
-  		</div>
-  	</div>
-  	<br>
+    </div>
+    <br>
+    <div class="image_display_r">
+      <img class="img-fluid"  alt="A scenic home image" src="/images/LSIDEA.JPG" style="max-height:100%; width: 100%; display: block;">
+      <div class="image_display_text_container">
+        <p>Custom Design!</p>
+      </div>
+    </div>
+    <br>
   </div>
 @endsection
