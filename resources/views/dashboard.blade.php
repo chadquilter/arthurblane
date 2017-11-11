0f9a5f1a78dd9dfd0663c8b120029c2f191d3976
Chad Quilter <chadquilter@hotmail.com>
2017-11-10 23:21:26 -0600

    update dahsboard look

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -37,10 +37,10 @@
 
 
                         <div class="row">
-                          <div class="col-10">
+                          <div class="col-8">
                             <strong>Job: </strong>{{$job->job_title}}
                           </div>
-                          <div class="col-2" role="group" aria-label="Job List">
+                          <div class="col-4" role="group" aria-label="Job List">
                             {!!Form::open(['action' => ['JobsController@destroy', $job->job_id], 'method' => 'POST', 'class' => 'pull-right'])!!}
                             {{Form::hidden('_method', 'DELETE')}}
                             <a href="/jobs/{{$job->job_id}}/edit" class="btn btn-primary">Edit</a>
