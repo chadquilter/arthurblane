983128ed6e45f4b38d0364662406e2ae7ceefaae
Chad Quilter <chadquilter@hotmail.com>
2017-11-10 22:11:23 -0600

    update dahsboard look

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -66,7 +66,7 @@
 
                 @foreach($quotes as $quote)
 
-                  <div class="row">
+                  <div class="row row-fluid show-grid">
                     <div class="col-12 col-md-8">
                       <strong>Job: </strong>{{$quote->title}}
                     </div>
