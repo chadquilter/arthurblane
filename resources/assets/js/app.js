6796c3bf8caba57924727dc6a207b800382c0a5f
Chad Quilter <chadquilter@hotmail.com>
2017-11-11 22:40:16 -0600

    update

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -23,8 +23,10 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+//Vue.component('example', require('./components/Example.vue'));
 
 const app = new Vue({
-    el: '#app'
+  data: {
+    message: 'Hello Vue!'
+  }
 });
