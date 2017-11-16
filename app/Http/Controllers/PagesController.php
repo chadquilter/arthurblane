ea4d8d3e49832b61394dc7052238e07749feef26
Chad Quilter <chadquilter@hotmail.com>
2017-11-16 17:30:43 -0600

    add all new updates for servies and add json dependency

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,21 +8,26 @@ use App\Service;
 class PagesController extends Controller
 {
 
-    public function __construct()
-    {
+    public function index(){
       $title = 'Welcome to '.config('app.name');
       $services = Service::orderBy('service_name', 'asc')->pluck('service_name', 'id');
-    }
-
-    public function index(){
 	//al method to pass values to page
        //return view('pages.index', compact('title'));
        return view('pages.index')
         ->with('title', $title)
         ->with('services', $services);
+
+
+        return view('dashboard')
+          ->with('jobs', $jobs)
+          ->with('user', $user)
+          ->with('quotes', $quotes)
+          ->with('addresses', $addresses);
     }
 
     public function quote(){
+      $title = 'Welcome to '.config('app.name');
+      $services = Service::orderBy('service_name', 'asc')->pluck('service_name', 'id');
        return view('pages.quote')
         ->with('title', $title)
         ->with('services', $services);
@@ -30,12 +35,15 @@ class PagesController extends Controller
 
     public function services(){
       $service_listing = Service::orderBy('service_name', 'asc')->pluck('service_name');
+      $services = Service::orderBy('service_name', 'asc')->pluck('service_name', 'id');
       $data = array(
 		       'title' => 'Services',
 		       'services' => $service_listing
       );
 
-      return view('pages.services')->with($data);
+      return view('pages.services')
+        ->with('services', $services),
+        ->with('data', $data);
     }
 
 }
