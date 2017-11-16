c5791cda8ececd9baea82f838c595a6b7de85e78
Chad Quilter <chadquilter@hotmail.com>
2017-11-16 17:16:10 -0600

    add all new updates for servies and add json dependency

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,16 +7,25 @@ use App\Service;
 
 class PagesController extends Controller
 {
+
+    public function __construct()
+    {
+      $title = 'Welcome to '.config('app.name')
+      $services = Service::orderBy('service_name', 'asc')->pluck('service_name', 'id');
+    }
+
     public function index(){
-       $title = 'Welcome to '.config('app.name');
 	//al method to pass values to page
        //return view('pages.index', compact('title'));
-       return view('pages.index')->with('title', $title);
+       return view('pages.index')
+        ->with('title', $title)
+        ->with('services', $services);
     }
 
     public function quote(){
-       $title = 'Welcome to '.config('app.name');
-       return view('pages.quote')->with('title', $title);
+       return view('pages.quote')
+        ->with('title', $title)
+        ->with('services', $services);
     }
 
     public function services(){
