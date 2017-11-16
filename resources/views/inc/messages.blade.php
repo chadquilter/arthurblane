5748e1065aef3917c3cbe90e8e628f3543e22bce
Chad Quilter <chadquilter@hotmail.com>
2017-11-15 19:46:12 -0600

    dashboard updtes

--- resources/views/inc/messages.blade.php
+++ resources/views/inc/messages.blade.php
@@ -1,19 +1,19 @@
 @if(count($errors) > 0)
     @foreach($errors->all() as $error)
-        <div class="alert alert-danger image_display_text_container">
+        <div class="alert alert-danger image_display_text_container" role="alert">
             {{$error}}
         </div>
     @endforeach
 @endif
 
 @if(session('success'))
-    <div class="alert alert-success image_display_text_container">
+    <div class="alert alert-success image_display_text_container" role="alert">
         {{session('success')}}
     </div>
 @endif
 
 @if(session('error'))
-    <div class="alert alert-danger image_display_text_container">
+    <div class="alert alert-danger image_display_text_container" role="alert">
         {{session('error')}}
     </div>
 @endif
