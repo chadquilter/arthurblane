2943ff9f289a7db8c7d4b5c04b390660471e49d5
Chad Quilter <chadquilter@hotmail.com>
2017-11-16 16:23:58 -0600

    update service table with column and preloaded crap

--- database/migrations/2017_11_16_210731_create_services_table.php
+++ database/migrations/2017_11_16_210731_create_services_table.php
@@ -15,8 +15,33 @@ class CreateServicesTable extends Migration
     {
         Schema::create('services', function (Blueprint $table) {
             $table->increments('id');
+            $table->mediumText('service_name');
             $table->timestamps();
         });
+
+        DB::table('users')->insert(
+          array(
+            'service_name' => 'Custom Homes'
+          ),(
+            'service_name' => 'Custom Concrete'
+          ),(
+            'service_name' => 'Custom Kitchen and Bath'
+          ),(
+            'service_name' => 'Construction Management'
+          ),(
+            'service_name' => 'Excavation'
+          ),(
+            'service_name' => 'Concrete and Asphalt (structural and paving)'
+          ),(
+            'service_name' => 'Structural Steel'
+          ),(
+            'service_name' => 'Interior Finish Out'
+          ),(
+            'service_name' => 'Ground Up Construction'
+          ),(
+            'service_name' => 'Demolition'
+          )
+        );
     }
 
     /**
