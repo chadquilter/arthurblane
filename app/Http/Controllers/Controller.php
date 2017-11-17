8655da21c95b444389c8e09693944909aa0f688a
Chad Quilter <chadquilter@hotmail.com>
2017-11-16 19:58:51 -0600

    add all new updates for servies and add json dependency

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,10 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use App\Service;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public static $mdg_services = Service::orderBy('service_name', 'asc')->pluck('service_name', 'id');
 }
