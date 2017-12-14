f9e1982ac29e355ae4771c2f6f8e3da978218c1d
Chad Quilter <chadquilter@hotmail.com>
2017-12-13 21:23:28 -0600

    update dashboard

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -52,8 +52,10 @@
       <div class="col-md-4">
         <div class="jumbotron">
           <p class="lead">
-            {{ config('app.name', 'MDG') }} has helped thousands of happy homeowners across Texas build the new custom-designed home of their dreams.
-            We build "eco-friendly" green custom homes of all sizes for all budgets. Whatever custom home you have in mind, from a quaint cottage in Austin,
+            <strong>{{ config('app.name', 'MDG') }}</strong> has helped thousands of happy homeowners across Texas build the new custom-designed home of their dreams.
+            We build "eco-friendly" green custom homes of all sizes for all budgets.
+            <br>
+            <strong>W</strong>hatever custom home you have in mind, from a quaint cottage in Austin,
             a rambling farm house in the Hill Country or an elegant luxury estate home in Houston, Dallas or San Antonio, we can build, repair, or add to it at an affordable price.
           </p>
         </div>
