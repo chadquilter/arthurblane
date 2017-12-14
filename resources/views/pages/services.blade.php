2f69b48aef72ba348d67ed1a38b40718e13109f0
Chad Quilter <chadquilter@hotmail.com>
2017-12-14 01:38:55 -0600

    update dashboard

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -36,7 +36,7 @@
 				<div class="image_display_r">
 					<img class="img-fluid" alt="A scenic home image" src="/images/ARCH1.JPG" style="max-height:100%; width: 100%; display: block;">
 					<div class="image_display_text_container">
-						<p>Custom Extensions/Overhauls for Rooms and Roofing!</p>
+						<p>Custom Roof and Room Extension!</p>
 					</div>
 				</div>
 
