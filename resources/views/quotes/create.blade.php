5dae69c08254287bf9bc5b8ccfa79756029658e1
Chad Quilter <chadquilter@hotmail.com>
2017-12-14 03:12:20 -0600

    update dashboard

--- resources/views/quotes/create.blade.php
+++ resources/views/quotes/create.blade.php
@@ -27,7 +27,7 @@
       <div class="container">
         <h1 class="display-2">Feel free </h1> to send us a request for a Quote or any Messages or Questions!
         <br>
-        We will be happy to help you in any way we can.:
+        We will be happy to help you in any way we can.
         <div class="card mx-auto border border-secondary">
           <div class="card-body">
             @include('inc.messages')
