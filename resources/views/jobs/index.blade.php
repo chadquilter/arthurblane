7877fbcbfc3f4fd565256b901e1b676a3ef22d73
Chad Quilter <chadquilter@hotmail.com>
2017-12-14 04:34:23 -0600

    update dashboard

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -6,17 +6,15 @@
   <div>
     @if(count($jobs) > 0)
         @foreach($jobs as $job)
-            <div class="well">
-                <h3><a href="/jobs/{{$job->job_id}}"> {{$job->job_title}} </a> </h3>
-                <br>&nbsp
-                <div id="web_space">
+            <div class="container">
+              <div id="card">
+                <div class="card">
+                	<h1 class="card-title"><a href="/jobs/{{$job->job_id}}"> {{$job->job_title}} </a></h1>
+                	<div class="card-block">
                     {!!$job->job_summary!!}
-                    <center>
-                        &nbsp</br>
-                        <hr>
-                        <small>Job Created: {{$job->created_at}} By: {{$job->user->name}} </small>
-                    </center>
+                  </div>
                 </div>
+              </div>
             </div>
         @endforeach
         {{$jobs->links()}}
