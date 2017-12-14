d4350c1d1a6ce1ac1bc06aeb3c704b04d4b4ab8a
Chad Quilter <chadquilter@hotmail.com>
2017-12-13 20:55:39 -0600

    update dashboard

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -4,7 +4,7 @@
 <br>
 <div class="container">
 		<div class="image_display_r">
-			<img class="img-fluid" alt="A scenic home image" src="/images/2_orig.jpg" style="max-height:50%; width: 50%; display: block;">
+			<img class="img-fluid" alt="A scenic home image" src="/images/2_orig.jpg" style="max-height:100%; width: 100%; display: block;">
 			<div class="image_display_text_container">
 				<p>Custom Bathroom and Counters!</p>
 			</div>
