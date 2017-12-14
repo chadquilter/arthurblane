ef35b917a1233b13f6323f0808b951a06b6dc35b
Chad Quilter <chadquilter@hotmail.com>
2017-12-13 22:41:37 -0600

    update dashboard

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -48,6 +48,16 @@
             <p>Custom Metal Work!</p>
           </div>
         </div>
+        <div class="jumbotron">
+          <p class="lead">
+            <strong>Whatever Custom Home </strong> you have in mind, from a quaint cottage in Austin,
+            a rambling farm house in the Hill Country or an elegant luxury estate home in Houston, Dallas or San Antonio,
+            we can build, repair, or add to it at an affordable price.
+          </p>
+          <p class="lead">
+            With over 10 years of building experience based in the center of Texas, you can be rest assured that our work is a cut above the rest!
+          </p>
+        </div>
       </div>
       <div class="col">
         <div class="jumbotron">
