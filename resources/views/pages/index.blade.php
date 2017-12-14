c78d2b1c4ea1375b20b581038ac0d5ebada8d441
Chad Quilter <chadquilter@hotmail.com>
2017-12-13 22:21:52 -0600

    update dashboard

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -67,16 +67,17 @@
     </div>
     <br>
     <div class="row">
-      <div class="jumbotron">
-        <p class="lead">
-          <strong>Whatever Custom Home </strong> you have in mind, from a quaint cottage in Austin,
-          a rambling farm house in the Hill Country or an elegant luxury estate home in Houston, Dallas or San Antonio,
-          we can build, repair, or add to it at an affordable price.
-        </p>
-        <br>
-        <p class="lead">
-          With over 10 years of building experience based in the center of Texas, you can be rest assured that our work is a cut above the rest!
-        </p>
+      <div class="col-md-8">
+        <div class="jumbotron">
+          <p class="lead">
+            <strong>Whatever Custom Home </strong> you have in mind, from a quaint cottage in Austin,
+            a rambling farm house in the Hill Country or an elegant luxury estate home in Houston, Dallas or San Antonio,
+            we can build, repair, or add to it at an affordable price.
+          </p>
+          <p class="lead">
+            With over 10 years of building experience based in the center of Texas, you can be rest assured that our work is a cut above the rest!
+          </p>
+        </div>
       </div>
     </div>
     <br>
