303aab0cfe0a09adb596e4d57f689c7edf0b9f10
Chad Quilter <chadquilter@hotmail.com>
2017-12-13 23:44:32 -0600

    update dashboard

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -67,6 +67,46 @@
 		<div class="row">
 			<div class="col-md-6">
 				<div class="image_display_r">
+					<img class="img-fluid" alt="A scenic home image" src="/images/5_orig.jpg" style="max-height:100%; width: 100%; display: block;">
+					<div class="image_display_text_container">
+						<p>Custom Repair Siding and Damage!</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="image_display_r">
+					<img class="img-fluid" alt="A scenic home image" src="/images/4_orig.jpg" style="max-height:100%; width: 100%; display: block;">
+					<div class="image_display_text_container">
+						<p>Custom Outdoor Rock Paths and Stairs!</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<br>
+		<div class="row">
+			<div class="col-md-6">
+				<div class="image_display_r">
+					<img class="img-fluid" alt="A scenic home image" src="/images/5_orig.jpg" style="max-height:100%; width: 100%; display: block;">
+					<div class="image_display_text_container">
+						<p>Custom Repair Siding and Damage!</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="image_display_r">
+					<img class="img-fluid" alt="A scenic home image" src="/images/4_orig.jpg" style="max-height:100%; width: 100%; display: block;">
+					<div class="image_display_text_container">
+						<p>Custom Outdoor Rock Paths and Stairs!</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<br>
+		<div class="row">
+			<div class="col-md-6">
+				<div class="image_display_r">
 					<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_BEDROOM1.JPG" style="max-height:100%; width: 100%; display: block;">
 					<div class="image_display_text_container">
 						<p>Custom Vanity!</p>
