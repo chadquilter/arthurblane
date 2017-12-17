f811096efdf50fa7ff653e8c4ac927e35f6db357
Chad Quilter <chadquilter@hotmail.com>
2017-12-17 00:06:55 -0600

    fix new page methods

--- resources/views/pages/asphalt.blade.php
+++ resources/views/pages/asphalt.blade.php
@@ -6,7 +6,7 @@
 		<div class="image_display_r">
 			<img class="img-fluid" alt="A scenic home image" src="/images/3_orig.jpg" style="max-height:100%; width: 100%; display: block;">
 			<div class="image_display_text_container">
-				<p>Custom Built Outdoor walkways and Paths!</p>
+				<p>Custom Asphalt!</p>
 			</div>
 		</div>
 		<br>
