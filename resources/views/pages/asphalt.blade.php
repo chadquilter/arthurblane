9ab67872a878608e4cf1caac7ae1cbed1de95461
Chad Quilter <chadquilter@hotmail.com>
2017-12-17 01:22:01 -0600

    fix new page methods

--- resources/views/pages/asphalt.blade.php
+++ resources/views/pages/asphalt.blade.php
@@ -9,9 +9,11 @@
 		<div class="row">
 			<div class="col-sm-2 alert alert-primary">
 				<strong class="display-1 badge badge-secondary">Bam:</strong>
+				<h1>Example heading <span class="badge badge-secondary">New</span></h1>
 			</div>
 			<div class="col-sm-10 alert alert-light image_display_r">
 				<strong class="display-3"> {{ $title }} </strong>
+				<h1>Example heading <span class="badge badge-secondary">New</span></h1>
 			</div>
 		</div>
 		<div class="image_display_r">
