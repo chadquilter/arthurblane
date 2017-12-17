c140f02c62299afa102135aabdcf9124acc8a04c
Chad Quilter <chadquilter@hotmail.com>
2017-12-16 23:06:32 -0600

    add new service pages and update page controller along with routing

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -176,34 +176,4 @@ class PagesController extends Controller
         ->with('services', $service_listing);
     }
 
-    public function services(){
-      $title = 'Welcome to '.config('app.name');
-      $service_listing = Service::orderBy('service_name', 'asc')->pluck('service_name');
-      $mdg_services = Service::orderBy('service_name', 'asc')->pluck('service_name', 'id');
-      $data = array(
-           'title' => 'Services',
-           'services' => $service_listing
-      );
-
-      return view('pages.services')
-        ->with('title', $title)
-        ->with('mdg_services', $mdg_services)
-        ->with('services', $service_listing);
-    }
-
-    public function services(){
-      $title = 'Welcome to '.config('app.name');
-      $service_listing = Service::orderBy('service_name', 'asc')->pluck('service_name');
-      $mdg_services = Service::orderBy('service_name', 'asc')->pluck('service_name', 'id');
-      $data = array(
-           'title' => 'Services',
-           'services' => $service_listing
-      );
-
-      return view('pages.services')
-        ->with('title', $title)
-        ->with('mdg_services', $mdg_services)
-        ->with('services', $service_listing);
-    }
-
 }
