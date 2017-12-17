938c3ff953c779f85e264a89068d30a069c9c66d
Chad Quilter <chadquilter@hotmail.com>
2017-12-17 01:24:45 -0600

    fix new page methods

--- resources/views/pages/asphalt.blade.php
+++ resources/views/pages/asphalt.blade.php
@@ -8,12 +8,10 @@
 	<div class="jumbotron">
 		<div class="row">
 			<div class="col-sm-2 alert alert-primary">
-				<strong class="display-1 badge badge-secondary">Bam:</strong>
-				<h1>Example heading <span class="badge badge-secondary">New</span></h1>
+				<h1><span class="badge badge-secondary">BAM!</span></h1>
 			</div>
 			<div class="col-sm-10 alert alert-light image_display_r">
 				<strong class="display-3"> {{ $title }} </strong>
-				<h1>Example heading <span class="badge badge-secondary">New</span></h1>
 			</div>
 		</div>
 		<div class="image_display_r">
