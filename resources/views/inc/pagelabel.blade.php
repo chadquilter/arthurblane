d92668656ade3643579699886bf9b04a9bd50317
Chad Quilter <chadquilter@hotmail.com>
2017-12-17 03:21:46 -0600

    add new inc file for page label

---
+++ resources/views/inc/pagelabel.blade.php
@@ -0,0 +1,8 @@
+<div class="row">
+  <div class="col-sm-2 alert alert-primary">
+    <h1><span class="badge badge-secondary">BAM!</span></h1>
+  </div>
+  <div class="col-sm-10 alert alert-light image_display_r">
+    <strong class="display-3"> {{ $title }} </strong>
+  </div>
+</div>
