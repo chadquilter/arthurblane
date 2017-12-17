6d613b847cd450808595fdbcb66a3ede1481f67d
Chad Quilter <chadquilter@hotmail.com>
2017-12-16 22:10:53 -0600

    add css class

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -10,10 +10,10 @@
       <div class="card">
         <div class="card-title">
           <div class="row">
-            <div class="col-sm-2">
+            <div class="col-sm-4">
               <h1 class="alert alert-success"> Job: </h1>
             </div>
-            <div class="col">
+            <div class="col-sm-8">
               <h1 class="alert alert-info"> {{$job->job_title}} </h1>
             </div>
           </div>
