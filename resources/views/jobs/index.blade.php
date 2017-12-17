1c9d3492725b16e7592c0d1ae3fda448494fa1ee
Chad Quilter <chadquilter@hotmail.com>
2017-12-17 04:57:13 -0600

    updates from adding new labeinc file to controller

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -9,20 +9,20 @@
       @php
         $title = $job->job_title
       @endphp
-    <div class="jumbotron">
-      <div class="card">
-        <div class="card-title ">
-          @include('inc.pagelabel')
+      <div class="jumbotron">
+        <div class="card">
+          <div class="card-title ">
+            @include('inc.pagelabel')
+          </div>
+          <div class="card-block">
+            {!!$job->job_summary!!}
+          </div>
+          <br>
+          {{$jobs->links()}}
         </div>
-        <div class="card-block">
-          {!!$job->job_summary!!}
-        </div>
-        <br>
       </div>
-    </div>
-    @endforeach
-    {{$jobs->links()}}
-    @else
+      @endforeach
+      @else
     <div class="card-title">
       <p>No current jobs posted.</p>
     </div>
