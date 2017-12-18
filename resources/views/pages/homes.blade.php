88764c2ff661b863ace185167355042bf6295c40
Chad Quilter <chadquilter@hotmail.com>
2017-12-18 16:08:02 -0600

    update kitchen bath page

--- resources/views/pages/homes.blade.php
+++ resources/views/pages/homes.blade.php
@@ -36,7 +36,7 @@
 			</div>
 			<div class="col">
 				<div class="image_display_r">
-					<img class="img-fluid" alt="A scenic home image" src="/images/ARCH1.JPG" style="max-height:100%; width: 100%; display: block;">
+					<img class="img-fluid" alt="A scenic home image" src="/images/homes/home2.JPG" style="max-height:100%; width: 100%; display: block;">
 					<div class="image_display_text_container">
 						<p>Custom Roof and Room Extension!</p>
 					</div>
@@ -46,105 +46,26 @@
 			</div>
 		</div>
 
-		<br>
-		<div class="row">
-			<div class="col">
-				<div class="image_display_r">
-					<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/CustomShower.jpg" style="max-height:100%; width: 100%; display: block;">
-					<div class="image_display_text_container">
-						<p>Custom Designed Shower!</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="image_display_r">
-					<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_BATHROOM1.JPG" style="max-height:100%; width: 100%; display: block;">
-					<div class="image_display_text_container">
-						<p>Custom Full Bath!</p>
-					</div>
-				</div>
-			</div>
-		</div>
 
 		<br>
-		<div class="row">
-			<div class="col">
 				<div class="image_display_r">
-					<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/window1.jpg" style="max-height:100%; width: 100%; display: block;">
-					<div class="image_display_text_container">
-						<p>Room Renovation!</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="image_display_r">
-					<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/Cub1.jpg" style="max-height:100%; width: 100%; display: block;">
-					<div class="image_display_text_container">
-						<p>Custom Cupbord Remodeling!</p>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<br>
-		<div class="row">
-			<div class="col">
-				<div class="image_display_r">
-					<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_44444.jpg" style="max-height:100%; width: 100%; display: block;">
-					<div class="image_display_text_container">
-						<p>Damage Repair!</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="image_display_r">
-					<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/hall.jpg" style="max-height:100%; width: 100%; display: block;">
-					<div class="image_display_text_container">
-						<p>Custom Remodeling!</p>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<br>
-		<div class="row">
-			<div class="col">
-				<div class="image_display_r">
-					<img class="img-fluid" alt="A scenic home image" src="/images/5_orig.jpg" style="max-height:100%; width: 100%; display: block;">
+					<img class="img-fluid" alt="A scenic home image" src="/images/homes/home1.jpg" style="max-height:100%; width: 100%; display: block;">
 					<div class="image_display_text_container">
 						<p>Custom Repair Siding and Damage!</p>
 					</div>
 				</div>
-			</div>
-			<div class="col">
-				<div class="image_display_r">
-					<img class="img-fluid" alt="A scenic home image" src="/images/4_orig.jpg" style="max-height:100%; width: 100%; display: block;">
-					<div class="image_display_text_container">
-						<p>Custom Outdoor Rock Steps!</p>
-					</div>
-				</div>
-			</div>
-		</div>
 
 		<br>
-		<div class="row">
-			<div class="col">
 				<div class="image_display_r">
-					<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_BEDROOM1.JPG" style="max-height:100%; width: 100%; display: block;">
+					<img class="img-fluid" alt="A scenic home image" src="/images/homes/home3.jpg" style="max-height:100%; width: 100%; display: block;">
 					<div class="image_display_text_container">
-						<p>Custom Vanity!</p>
+						<p>Custom Repair Siding and Damage!</p>
 					</div>
 				</div>
-			</div>
-			<div class="col">
-				<div class="image_display_r">
-					<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_BEDROOM1A.JPG" style="max-height:100%; width: 100%; display: block;">
-					<div class="image_display_text_container">
-						<p>Custom Ceiling and Molding!</p>
-					</div>
-				</div>
-			</div>
-		</div>
+
+
+
+
 		<br>
 	</div>
 </div>
