d737e6c46e64d9a6e73ccd55911e84f83ba22db8
Chad Quilter <chadquilter@hotmail.com>
2017-12-18 15:53:48 -0600

    update kitchen bath page

--- resources/views/pages/concrete.blade.php
+++ resources/views/pages/concrete.blade.php
@@ -37,9 +37,9 @@
 			</div>
 			<div class="col">
 				<div class="image_display_r">
-					<img class="img-fluid" alt="A scenic home image" src="/images/ARCH1.JPG" style="max-height:100%; width: 100%; display: block;">
+					<img class="img-fluid" alt="A scenic home image" src="/images/asphalt/concrete1z.jpg" style="max-height:100%; width: 100%; display: block;">
 					<div class="image_display_text_container">
-						<p>Custom Roof and Room Extension!</p>
+						<p>Custom bridge!</p>
 					</div>
 				</div>
 
@@ -47,105 +47,6 @@
 			</div>
 		</div>
 
-		<br>
-		<div class="row">
-			<div class="col">
-				<div class="image_display_r">
-					<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/CustomShower.jpg" style="max-height:100%; width: 100%; display: block;">
-					<div class="image_display_text_container">
-						<p>Custom Designed Shower!</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="image_display_r">
-					<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_BATHROOM1.JPG" style="max-height:100%; width: 100%; display: block;">
-					<div class="image_display_text_container">
-						<p>Custom Full Bath!</p>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<br>
-		<div class="row">
-			<div class="col">
-				<div class="image_display_r">
-					<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/window1.jpg" style="max-height:100%; width: 100%; display: block;">
-					<div class="image_display_text_container">
-						<p>Room Renovation!</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="image_display_r">
-					<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/Cub1.jpg" style="max-height:100%; width: 100%; display: block;">
-					<div class="image_display_text_container">
-						<p>Custom Cupbord Remodeling!</p>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<br>
-		<div class="row">
-			<div class="col">
-				<div class="image_display_r">
-					<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_44444.jpg" style="max-height:100%; width: 100%; display: block;">
-					<div class="image_display_text_container">
-						<p>Damage Repair!</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="image_display_r">
-					<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/hall.jpg" style="max-height:100%; width: 100%; display: block;">
-					<div class="image_display_text_container">
-						<p>Custom Remodeling!</p>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<br>
-		<div class="row">
-			<div class="col">
-				<div class="image_display_r">
-					<img class="img-fluid" alt="A scenic home image" src="/images/5_orig.jpg" style="max-height:100%; width: 100%; display: block;">
-					<div class="image_display_text_container">
-						<p>Custom Repair Siding and Damage!</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="image_display_r">
-					<img class="img-fluid" alt="A scenic home image" src="/images/4_orig.jpg" style="max-height:100%; width: 100%; display: block;">
-					<div class="image_display_text_container">
-						<p>Custom Outdoor Rock Steps!</p>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<br>
-		<div class="row">
-			<div class="col">
-				<div class="image_display_r">
-					<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_BEDROOM1.JPG" style="max-height:100%; width: 100%; display: block;">
-					<div class="image_display_text_container">
-						<p>Custom Vanity!</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="image_display_r">
-					<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_BEDROOM1A.JPG" style="max-height:100%; width: 100%; display: block;">
-					<div class="image_display_text_container">
-						<p>Custom Ceiling and Molding!</p>
-					</div>
-				</div>
-			</div>
-		</div>
 		<br>
 	</div>
 </div>
