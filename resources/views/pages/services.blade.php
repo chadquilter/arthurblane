5489aa396f83fdd08741ef5ae57f0268524d053b
Chad Quilter <chadquilter@hotmail.com>
2017-12-19 04:48:37 -0600

    redirect new registrations back to home

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -5,7 +5,7 @@
 	<div class="container">
 		@include('inc.pagelabel')
 		<div class="card image_display_r">
-			<img class="card-img-top" alt="A scenic home image" src="/images/2_orig.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
+			<img class="card-img-top" src="/images/2_orig.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
 			<div class="card-body">
 				<div class="card-title">
 					<p>Custom Built Outdoor Walkways and Paths!</p>
@@ -49,125 +49,6 @@
 			</div>
 		</div>
 
-		<br>
-		<div class="row">
-			<div class="col">
-				<div class="card image_display_r">
-					<img class="card-img-top" alt="A scenic home image" src="/images/mdg_images/CustomShower.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
-					<div class="card-body">
-						<div class="card-title">
-							<p>Custom Designed Shower!</p>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="card image_display_r">
-					<img class="card-img-top" alt="A scenic home image" src="/images/mdg_images/IMG_BATHROOM1.JPG" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
-					<div class="card-body">
-						<div class="card-title">
-							<p>Custom Full Bath!</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<br>
-		<div class="row">
-			<div class="col">
-				<div class="card image_display_r">
-					<img class="card-img-top" alt="A scenic home image" src="/images/mdg_images/window1.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
-					<div class="card-body">
-						<div class="card-title">
-							<p>Room Renovation!</p>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="card image_display_r">
-					<img class="card-img-top" alt="A scenic home image" src="/images/mdg_images/Cub1.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
-					<div class="card-body">
-						<div class="card-title">
-							<p>Custom Cupbord Remodeling!</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<br>
-		<div class="row">
-			<div class="col">
-				<div class="card image_display_r">
-					<img class="card-img-top" alt="A scenic home image" src="/images/mdg_images/IMG_44444.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
-					<div class="card-body">
-						<div class="card-title">
-							<p>Damage Repair!</p>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="card image_display_r">
-					<img class="card-img-top" alt="A scenic home image" src="/images/mdg_images/hall.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
-					<div class="card-body">
-						<div class="card-title">
-							<p>Custom Remodeling!</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<br>
-		<div class="row">
-			<div class="col">
-				<div class="card image_display_r">
-					<img class="card-img-top" alt="A scenic home image" src="/images/5_orig.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
-					<div class="card-body">
-						<div class="card-title">
-							<p>Custom Repair Siding and Damage!</p>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="card image_display_r">
-					<img class="card-img-top" alt="A scenic home image" src="/images/4_orig.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
-					<div class="card-body">
-						<div class="card-title">
-							<p>Custom Outdoor Rock Steps!</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<br>
-		<div class="row">
-			<div class="col">
-				<div class="card image_display_r">
-					<img class="card-img-top" alt="A scenic home image" src="/images/mdg_images/IMG_BEDROOM1.JPG" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
-					<div class="card-body">
-						<div class="card-title">
-							<p>Custom Vanity!</p>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="card image_display_r">
-					<img class="card-img-top" alt="A scenic home image" src="/images/mdg_images/IMG_BEDROOM1A.JPG" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
-					<div class="card-body">
-						<div class="card-title">
-							<p>Custom Ceiling and Molding!</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
 
 		<br>
 
