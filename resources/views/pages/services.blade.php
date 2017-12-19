7c29d1de0d547878b5e4c0083e2837a33c1b3e17
Chad Quilter <chadquilter@hotmail.com>
2017-12-19 05:06:07 -0600

    redirect new registrations back to home

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -88,7 +88,30 @@
 			</div>
 		</div>
 
+		<br>
+		<div class="row">
+			<div class="col">
+				<div class="card image_display_r">
+					<img class="card-img-top" alt="A scenic home image" src="/images/steel/steel_barn4.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
+					<div class="card-body">
+						<div class="card-title">
+							<p>Custom Steel Barn Interior! </p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col">
 
+				<div class="card image_display_r">
+					<img class="card-img-top" alt="A scenic home image" src="/images/steel/steel_barn2.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
+					<div class="card-body">
+						<div class="card-title">
+							<p>Customized Steel Structures! </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
 	</div>
 @endsection
