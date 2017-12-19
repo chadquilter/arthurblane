86b33210ca78ce87a0948520aef4952d74a4fd48
Chad Quilter <chadquilter@hotmail.com>
2017-12-19 07:47:51 -0600

    fix service listing

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -19,7 +19,7 @@ class PagesController extends Controller
     }
 
     public function services(){
-      $title = config('app.name').' Services';
+      $title = 'Services';
       $mdg_services = Service::orderBy('service_name', 'asc')->where('service_type', '')->paginate(1000, ['*'], 'mdg_services');
 
       return view('pages.services')
