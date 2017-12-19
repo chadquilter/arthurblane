5fbe737c964eb80cc19e456b1948d2dacba79101
Chad Quilter <chadquilter@hotmail.com>
2017-12-18 21:10:14 -0600

    use service urls

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -29,182 +29,113 @@ class PagesController extends Controller
 
     public function asphalt(){
       $title = 'Concrete and Asphalt';
-      $service_listing = Service::orderBy('service_name', 'asc')->pluck('service_name');
       $mdg_services = Service::orderBy('service_name', 'asc')
         ->where('service_type', 'asphalt')
         ->pluck('service_name', 'id');
 
-      $data = array(
-           'title' => 'Services',
-           'services' => $service_listing
-      );
-
       return view('pages.asphalt')
         ->with('title', $title)
-        ->with('mdg_services', $mdg_services)
-        ->with('services', $service_listing);
+        ->with('mdg_services', $mdg_services);
     }
 
     public function cm(){
       $title = 'Construction Management';
-      $service_listing = Service::orderBy('service_name', 'asc')->pluck('service_name');
       $mdg_services = Service::orderBy('service_name', 'asc')
         ->where('service_type', 'cm')
         ->pluck('service_name', 'id');
 
-      $data = array(
-           'title' => 'Services',
-           'services' => $service_listing
-      );
-
       return view('pages.cm')
         ->with('title', $title)
-        ->with('mdg_services', $mdg_services)
-        ->with('services', $service_listing);
+        ->with('mdg_services', $mdg_services);
     }
 
     public function concrete(){
       $title = 'Custom Concrete';
-      $service_listing = Service::orderBy('service_name', 'asc')->pluck('service_name');
       $mdg_services = Service::orderBy('service_name', 'asc')
         ->where('service_type', 'concrete')
         ->pluck('service_name', 'id');
 
-      $data = array(
-           'title' => 'Services',
-           'services' => $service_listing
-      );
-
       return view('pages.concrete')
         ->with('title', $title)
-        ->with('mdg_services', $mdg_services)
-        ->with('services', $service_listing);
+        ->with('mdg_services', $mdg_services);
     }
 
     public function kitchenbath(){
       $title = 'Kitchen and Bath';
-      $service_listing = Service::orderBy('service_name', 'asc')->pluck('service_name');
       $mdg_services = Service::orderBy('service_name', 'asc')
         ->where('service_type', 'kitchenbath')
         ->pluck('service_name', 'id');
 
-      $data = array(
-           'title' => 'Services',
-           'services' => $service_listing
-      );
-
       return view('pages.kitchenbath')
         ->with('title', $title)
-        ->with('mdg_services', $mdg_services)
-        ->with('services', $service_listing);
+        ->with('mdg_services', $mdg_services);
     }
 
     public function demolition(){
       $title = 'Demolition';
-      $service_listing = Service::orderBy('service_name', 'asc')->pluck('service_name');
       $mdg_services = Service::orderBy('service_name', 'asc')
         ->where('service_type', 'demolition')
         ->pluck('service_name', 'id');
 
-      $data = array(
-           'title' => 'Services',
-           'services' => $service_listing
-      );
-
       return view('pages.demolition')
         ->with('title', $title)
-        ->with('mdg_services', $mdg_services)
-        ->with('services', $service_listing);
+        ->with('mdg_services', $mdg_services);
     }
 
     public function excavation(){
       $title = 'Excavation';
-      $service_listing = Service::orderBy('service_name', 'asc')->pluck('service_name');
       $mdg_services = Service::orderBy('service_name', 'asc')
         ->where('service_type', 'excavation')
         ->pluck('service_name', 'id');
 
-      $data = array(
-           'title' => 'Services',
-           'services' => $service_listing
-      );
 
       return view('pages.excavation')
         ->with('title', $title)
-        ->with('mdg_services', $mdg_services)
-        ->with('services', $service_listing);
+        ->with('mdg_services', $mdg_services);
     }
 
     public function groundupconstruction(){
       $title = 'Ground Up Construction';
-      $service_listing = Service::orderBy('service_name', 'asc')->pluck('service_name');
       $mdg_services = Service::orderBy('service_name', 'asc')
         ->where('service_type', 'groundupconstruction')
         ->pluck('service_name', 'id');
 
-      $data = array(
-           'title' => 'Services',
-           'services' => $service_listing
-      );
-
       return view('pages.groundupconstruction')
         ->with('title', $title)
-        ->with('mdg_services', $mdg_services)
-        ->with('services', $service_listing);
+        ->with('mdg_services', $mdg_services);
     }
 
     public function finishout(){
       $title = 'Interior Finish-Out';
-      $service_listing = Service::orderBy('service_name', 'asc')->pluck('service_name');
       $mdg_services = Service::orderBy('service_name', 'asc')
         ->where('service_type', 'finishout')
         ->pluck('service_name', 'id');
 
-      $data = array(
-           'title' => 'Services',
-           'services' => $service_listing
-      );
-
       return view('pages.finishout')
         ->with('title', $title)
-        ->with('mdg_services', $mdg_services)
-        ->with('services', $service_listing);
+        ->with('mdg_services', $mdg_services);
     }
 
     public function steel(){
       $title = 'Structural Steel';
-      $service_listing = Service::orderBy('service_name', 'asc')->pluck('service_name');
       $mdg_services = Service::orderBy('service_name', 'asc')
         ->where('service_type', 'steel')
         ->pluck('service_name', 'id');
 
-      $data = array(
-		       'title' => 'Services',
-		       'services' => $service_listing
-      );
-
       return view('pages.steel')
         ->with('title', $title)
-        ->with('mdg_services', $mdg_services)
-        ->with('services', $service_listing);
+        ->with('mdg_services', $mdg_services);
     }
 
     public function homes(){
       $title = 'Custom Homes';
-      $service_listing = Service::orderBy('service_name', 'asc')->pluck('service_name');
       $mdg_services = Service::orderBy('service_name', 'asc')
         ->where('service_type', 'homes')
         ->pluck('service_name', 'id');
 
-      $data = array(
-           'title' => 'Services',
-           'services' => $service_listing
-      );
-
       return view('pages.homes')
         ->with('title', $title)
-        ->with('mdg_services', $mdg_services)
-        ->with('services', $service_listing);
+        ->with('mdg_services', $mdg_services);
     }
 
 }
