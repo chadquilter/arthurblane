53f3f468da702b013f8c4c0e500898923e210110
Chad Quilter <chadquilter@hotmail.com>
2017-12-19 05:12:31 -0600

    redirect new registrations back to home

--- resources/views/pages/steel.blade.php
+++ resources/views/pages/steel.blade.php
@@ -1,86 +1,85 @@
 @extends('layouts.front')
 
 @section('content')
-<br>
-<div class="container">
-	@include('inc.pagelabel')
-	<div class="card image_display_r">
+	<br>
+	<div class="container">
+		@include('inc.pagelabel')
+		<div class="card image_display_r">
 			<img class="card-img-top" src="/images/mdg_images/IMG_00111.jpg" alt="Card image cap">
 			<div class="card-body">
 				<h5 class="card-title">Customized Steel made to order!</h5>
 			</div>
 		</div>
-	</div>
-	<br>
+		<br>
 
-	<div class="row">
-		<div class="col">
-			<div class="jumbotron">
-				<h4 class="display-4">{{$title}}</h4>
+		<div class="row">
+			<div class="col">
+				<div class="jumbotron">
+					<h4 class="display-4">{{$title}}</h4>
+					<br>
+					include but are not limited to:
+					<hr class="my-4">
+					<p class="lead">
+						@if(count($mdg_services) > 0)
+							<ul>
+								@foreach($mdg_services as $mdg_service)
+									<li>{{$mdg_service}}</li>
+								@endforeach
+							</ul>
+						@else
+							<li>Our services listing are currently under construction!</li>
+						@endif
+					</p>
+					<hr class="my-4">
+				</div>
+			</div>
+			<div class="col">
+				<div class="card image_display_r">
+					<img class="card-img-top" alt="A scenic home image" src="/images/steel/steel_barn3.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
+					<div class="image_display_text_container">
+						<p>Custom Steel Pens!</p>
+					</div>
+				</div>
+
 				<br>
-				include but are not limited to:
-				<hr class="my-4">
-				<p class="lead">
-					@if(count($mdg_services) > 0)
-						<ul>
-							@foreach($mdg_services as $mdg_service)
-								<li>{{$mdg_service}}</li>
-							@endforeach
-						</ul>
-					@else
-						<li>Our services listing are currently under construction!</li>
-					@endif
-				</p>
-				<hr class="my-4">
 			</div>
 		</div>
-		<div class="col">
-			<div class="card image_display_r">
-				<img class="card-img-top" alt="A scenic home image" src="/images/steel/steel_barn3.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
-				<div class="image_display_text_container">
-					<p>Custom Steel Pens!</p>
+
+		<br>
+		<div class="row">
+			<div class="col">
+				<div class="card image_display_r">
+					<img class="card-img-top" alt="A scenic home image" src="/images/steel/steel_barn4.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
+					<div class="card-body">
+						<div class="card-title">
+							<p>Custom Steel Barn Interior! </p>
+						</div>
+					</div>
 				</div>
 			</div>
+			<div class="col">
 
-			<br>
-		</div>
-	</div>
-
-	<br>
-	<div class="row">
-		<div class="col">
-			<div class="card image_display_r">
-				<img class="card-img-top" alt="A scenic home image" src="/images/steel/steel_barn4.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
-				<div class="card-body">
-					<div class="card-title">
-						<p>Custom Steel Barn Interior! </p>
+				<div class="card image_display_r">
+					<img class="card-img-top" alt="A scenic home image" src="/images/steel/steel_barn2.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
+					<div class="card-body">
+						<div class="card-title">
+							<p>Customized Steel Structures! </p>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="col">
 
-			<div class="card image_display_r">
-				<img class="card-img-top" alt="A scenic home image" src="/images/steel/steel_barn2.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
-				<div class="card-body">
-					<div class="card-title">
-						<p>Customized Steel Structures! </p>
-					</div>
+		<br>
+
+		<div class="card image_display_r">
+			<img class="card-img-top" alt="A scenic home image" src="/images/steel/steel_barn1.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
+			<div class="card-body">
+				<div class="card-title">
+					<p>Custom Steel Structures! </p>
 				</div>
 			</div>
 		</div>
+
 	</div>
-
-	<br>
-
-	<div class="card image_display_r">
-		<img class="card-img-top" alt="A scenic home image" src="/images/steel/steel_barn1.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
-		<div class="card-body">
-			<div class="card-title">
-				<p>Custom Steel Structures! </p>
-			</div>
-		</div>
-	</div>
-
-</div>
 @endsection
