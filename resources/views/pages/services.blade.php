14eca8180ff834e23fe0bf66ce3afd266e707d74
Chad Quilter <chadquilter@hotmail.com>
2017-12-19 05:21:30 -0600

    redirect new registrations back to home

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -64,15 +64,6 @@
 			</div>
 		</div>
 
-		<br>
-		<div class="card image_display_r">
-			<img class="card-img-top" alt="A scenic home image" src="/images/homes/home3.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
-			<div class="card-body">
-				<div class="card-title">
-					<p>Custom Homes done Right!</p>
-				</div>
-			</div>
-		</div>
 
 		<br>
 		<div class="row">
@@ -98,6 +89,20 @@
 				</div>
 			</div>
 		</div>
+
 		<br>
+		<div class="card image_display_r">
+			<img class="card-img-top" alt="A scenic home image" src="/images/homes/home3.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
+			<div class="card-body">
+				<div class="card-title">
+					<p>Custom Homes done Right!</p>
+				</div>
+			</div>
+		</div>
+
+		<br>
+
+
+
 	</div>
 @endsection
