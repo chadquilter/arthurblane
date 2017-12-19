7510007bab7c61f0b83f2bbd16bb6575bd95a67b
Chad Quilter <chadquilter@hotmail.com>
2017-12-18 21:04:32 -0600

    use service urls

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -23,7 +23,7 @@
 						@if(count($mdg_services) > 0)
 							<ul>
 								@foreach($mdg_services as $mdg_id)
-									<li>{{ $mdg_id->service_url }} test</li>
+									<li><a href="/{{ $mdg_id->service_url }}">{{ $mdg_id->service_name }}</a></li>
 								@endforeach
 							</ul>
 						@else
