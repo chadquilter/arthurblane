f04643aa9d11e0dc88d59d66dec9b95463fbcae5
Chad Quilter <chadquilter@hotmail.com>
2017-12-20 04:54:28 -0600

    add new images all over

--- resources/views/pages/excavation.blade.php
+++ resources/views/pages/excavation.blade.php
@@ -37,11 +37,43 @@
 				</div>
 			</div>
 
-
 			<br>
 		</div>
 	</div>
 
+	<div class="col">
+		<div class="card image_display_r">
+			<img class="card-img-top" alt="A scenic home image" src="/images/excavation/excavation3.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
+			<div class="card-body">
+				<div class="card-title">
+					<p>Custom Cupbord Remodeling!</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col">
+			<div class="card image_display_r">
+				<img class="card-img-top" alt="A scenic home image" src="/images/excavation/excavation1.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
+				<div class="card-body">
+					<div class="card-title">
+						<p>Room Renovation!</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col">
+			<div class="card image_display_r">
+				<img class="card-img-top" alt="A scenic home image" src="/images/excavation/bobcat.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
+				<div class="card-body">
+					<div class="card-title">
+						<p>Custom Cupbord Remodeling!</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
 	<br>
 </div>
