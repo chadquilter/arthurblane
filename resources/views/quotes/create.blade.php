123e573371fea58b81cb65c0036cd6d63a8bd229
Chad Quilter <chadquilter@hotmail.com>
2017-12-20 04:33:06 -0600

    add new images all over

--- resources/views/quotes/create.blade.php
+++ resources/views/quotes/create.blade.php
@@ -49,9 +49,6 @@
                 <div class="card-title">
                   <div class="row">
                     <div class="col">
-                      <
-                    </div>
-                    <div class="col">
                       <strong> Mike Grounds </strong>
                     </div>
                   </div>
