0c20c1041b2c817dc1a5d0046671997a25fde19e
Chad Quilter <chadquilter@hotmail.com>
2017-12-20 07:11:01 -0600

    add new images all over

--- resources/views/inc/pagelabel.blade.php
+++ resources/views/inc/pagelabel.blade.php
@@ -1,5 +1,5 @@
 <div class="card">
-  <div class="card-body">
+  <div class="card-body-top">
 <div class="row">
   <div class="col-sm-4 alert alert-primary">
     <h4 class="display-4"><span class="badge badge-secondary">BAM!</span></h4>
