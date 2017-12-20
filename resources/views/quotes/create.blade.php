83a32db8e2d54161a943541f39000361c068ca28
Chad Quilter <chadquilter@hotmail.com>
2017-12-20 11:07:33 -0600

    add new custom scss file

--- resources/views/quotes/create.blade.php
+++ resources/views/quotes/create.blade.php
@@ -133,8 +133,9 @@
               {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
             </div>
             {!! Form::close() !!}
+            <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
             <script>
-            CKEDITOR.replace( 'article-ckeditor' );
+              CKEDITOR.replace( 'article-ckeditor' );
             </script>
           </div>
         </div>
