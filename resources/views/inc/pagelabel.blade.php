f8a96b03a7fc23761d07fe68bf685990bd46ab8b
Chad Quilter <chadquilter@hotmail.com>
2017-12-20 05:24:02 -0600

    add new images all over

--- resources/views/inc/pagelabel.blade.php
+++ resources/views/inc/pagelabel.blade.php
@@ -1,8 +1,8 @@
 <div class="row">
-  <div class="col-sm-2 alert alert-primary">
-    <h1><span class="badge badge-secondary">BAM!</span></h1>
+  <div class="col alert alert-primary">
+    <h1><div class="badge badge-secondary">BAM!</div></h1>
   </div>
-  <div class="col-sm-10 alert alert-light image_display_r">
+  <div class="col alert alert-light image_display_r">
     <h4 class="display-4"> {{ $title }} </h4>
   </div>
 </div>
