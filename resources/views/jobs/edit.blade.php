c978b6fca4311995d31cd9c947e604ae09cf04af
Chad Quilter <chadquilter@hotmail.com>
2017-12-27 02:29:27 -0600

    add new dashboard menu blade

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -106,7 +106,6 @@
                     @endif
                     <div class="col-md-2">
                       {{Form::label($job_option_id, $job_option_name)}}
-                      <br>
                       @if(count($bool_types) > 0)
                         @foreach($bool_types as $bool_id => $bool_name)
                           {{Form::radio($job_option_id, $bool_id, $job->job_option_id, ['class' => 'form-control'])}} {{$bool_name}}
