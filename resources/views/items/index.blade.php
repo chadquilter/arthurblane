c563425227d4961a1cf75c76901a5ec797ae103a
Chad Quilter <chadquilter@hotmail.com>
2017-12-27 00:54:10 -0600

    add new dashboard menu blade

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -7,17 +7,7 @@
       <div class="card mx-auto">
         <br>
         <div class="card-body">
-          <div class="btn-group" role="group" aria-label="links">
-            <a href="/dashboard" class="btn btn-primary">Back to Dashboard</a>
-          </div>
-          <div class="btn-group" role="group" aria-label="links">
-            <a href="/jobs/create" class="btn btn-success">Create Job</a>
-            <a href="/quotes" class="btn btn-success">Create Quote</a>
-          </div>
-          <div class="btn-group" role="group" aria-label="links">
-            <a href="/address" class="btn btn-warning">Manage Addresses</a>
-            <a href="/items/create" class="btn btn-warning">Create Item</a>
-          </div>
+          @include('inc.dashmenu')
           <hr class="my-1">
           @include('inc.messages')
           <br>
