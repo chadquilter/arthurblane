b19318e75132b076d24a7243a7e6ce4ded6db717
Chad Quilter <chadquilter@hotmail.com>
2017-12-27 17:39:25 -0600

    update admin nav bar

--- resources/views/inc/dashmenu.blade.php
+++ resources/views/inc/dashmenu.blade.php
@@ -12,15 +12,15 @@
           <a href="/quotes" class="alert alert-primary btn btn-secondary" role="button">Create Quote</a>
           <a href="/address" class="alert alert-primary btn btn-secondary" role="button">Manage Addresses</a>
           @if ( Request::route()->getName() == 'address.index')
-             <a href="/address/create" class="alert alert-primary btn btn-secondary" role="button">Return to Dashboard</a>
+             <a href="/address/create" class="alert alert-primary btn btn-secondary" role="button">Add Address</a>
           @endif
           <a href="/items" class="alert alert-primary btn btn-secondary" role="button">Manage Items</a>
           @if ( Request::route()->getName() == 'items.index')
-             <a href="/items/create" class="alert alert-primary btn btn-secondary" role="button">Return to Dashboard</a>
+             <a href="/items/create" class="alert alert-primary btn btn-secondary" role="button">Add Item</a>
           @endif
           <a href="/forms" class="alert alert-primary btn btn-secondary" role="button">Manage Proposals</a>
-          @if ( Request::route()->getName() == 'forms.index') 
-             <a href="/forms/create" class="alert alert-primary btn btn-secondary" role="button">Return to Dashboard</a>
+          @if ( Request::route()->getName() == 'forms.index')
+             <a href="/forms/create" class="alert alert-primary btn btn-secondary" role="button">Add Form</a>
           @endif
       </div>
     </div>
