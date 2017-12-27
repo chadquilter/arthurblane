d14cacbe3c81cf946c0ded03188c238c3a33ac2f
Chad Quilter <chadquilter@hotmail.com>
2017-12-27 03:24:20 -0600

    add new dashboard menu blade

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -90,7 +90,7 @@
               <div class="alert alert-info" role="alert">
                 <div class="row">
                 @foreach ($job_option_types as $job_option_id => $job_option_name)
-                    <div class="col">
+                    <div class="col-lg-3">
                       {{Form::label($job_option_id, $job_option_name)}}
                       @if(count($bool_types) > 0)
                       <div class="col">
