18dab1ee95643f035d8328e3b9afbec9434b908d
Chad Quilter <chadquilter@hotmail.com>
2017-12-27 00:51:26 -0600

    add new dashboard menu blade

--- resources/views/inc/dashmenu.blade.php
+++ resources/views/inc/dashmenu.blade.php
@@ -4,13 +4,11 @@
       <div class="card-body">
         <p class="lead"><strong>Dashboard Services:</strong></p>
         <hr class="my-4">
-        <div class="btn-group" role="group" aria-label="links">
           <a href="/jobs/create" class="alert alert-primary btn btn-secondary" role="button">Create Job</a>
           <a href="/quotes" class="alert alert-primary btn btn-secondary" role="button">Create Quote</a>
           <a href="/address" class="alert alert-primary btn btn-secondary" role="button">Manage Addresses</a>
           <a href="/items" class="alert alert-primary btn btn-secondary" role="button">Manage Items</a>
           <a href="/forms" class="alert alert-primary btn btn-secondary" role="button">Manage Proposals/Forms</a>
-        </div>
       </div>
     </div>
 </p>
