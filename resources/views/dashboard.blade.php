90da6547133bd824c46a914ff25373d6cb318ee9
Chad Quilter <chadquilter@hotmail.com>
2017-12-27 00:18:31 -0600

    add new dashboard menu blade

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,7 +20,7 @@
         @endif
         @if(count($jobs) > 0)
             <div id="jobs_div" name="jobs_div" class="container border border-secondary rounded shadow_only">
-              <p class="lead"><span class="badge badge-secondary">{{ $jobs->total() }}</span><h3 class="display-3"> Job Entries:</h3></p>
+              <p class="lead"><h3 class="display-3"><span class="badge badge-secondary">{{ $jobs->total() }}</span>Job Entries:</h3></p>
               <hr class="my-4">
               @foreach($jobs as $job)
                 <div class="row">
