f1a30175d826ff98c481827b037024156b72bc50
Chad Quilter <chadquilter@hotmail.com>
2017-12-26 23:56:12 -0600

    add new dashboard menu blade

--- resources/views/inc/dashmenu.blade.php
+++ resources/views/inc/dashmenu.blade.php
@@ -1,7 +1,7 @@
 <p class="lead">Dashboard Services:</p>
 <hr class="my-4">
 <p class="lead">
-    <div class="card image_display_r">
+    <div class="card image_display_r border border-secondary rounded shadow_only">
       <div class="card-body">
         <div class="btn-group" role="group" aria-label="links">
           <a href="/jobs/create" class="alert alert-primary btn btn-secondary" role="button">Create Job</a>
