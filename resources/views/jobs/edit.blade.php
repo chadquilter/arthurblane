4dd499bff8bf60cf4b9aac29d848720053965d27
Chad Quilter <chadquilter@hotmail.com>
2017-12-27 02:54:30 -0600

    add new dashboard menu blade

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -100,7 +100,7 @@
               <div class="alert alert-info" role="alert">
                 @foreach ($job_option_types as $job_option_id => $job_option_name)
 
-                  @if($loop->first || $loop->iteration === 6)
+                  @if($loop->first || $loop->iteration === 4)
                     <div class="row">
                     @endif
                     <div class="col-md-2">
@@ -117,7 +117,7 @@
                     @else
                       <h1>No Types Listed!</h1>
                     @endif
-                    @if($loop->iteration === 5 || $loop->iteration >= 10 || $loop->last)
+                    @if($loop->iteration === 3 || $loop->iteration >= 8 || $loop->last)
                     </div>
                   @endif
                 @endforeach
