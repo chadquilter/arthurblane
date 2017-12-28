a8a97ba702555b3327f884cdce27fcfcf4862f40
Chad Quilter <chadquilter@hotmail.com>
2017-12-28 08:43:06 -0600

    add delete warning modal to delete buttons

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -44,7 +44,7 @@
                             </button>
                           </div>
                           <div class="modal-body">
-                            Are you sure you wish to delete this Job?
+                            <p class="lead">Are you sure you wish to delete this Job?</p>
                           </div>
                           <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -91,7 +91,7 @@
                       <div class="modal-dialog" role="document">
                         <div class="modal-content">
                           <div class="modal-header">
-                            <h5 class="modal-title display-3" id="exampleModalLabel{{$quote->id}}">Delete Job!</h5>
+                            <h5 class="modal-title" id="exampleModalLabel{{$quote->id}}">Delete Quote!</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                               <span aria-hidden="true">&times;</span>
                             </button>
