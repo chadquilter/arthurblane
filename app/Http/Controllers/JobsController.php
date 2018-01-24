0d9252254b12573a5547fb32d863ed9f736e92bb
Chad Quilter <chadquilter@gmail.com>
2018-01-24 09:52:20 -0600

    add dir lookup to store

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -213,7 +213,7 @@ class JobsController extends Controller
         $mdg_services = Service::orderBy('service_name', 'asc')->pluck('service_name', 'id');
         //$mdg_services_1 = Job::find($id)->services()->orderBy('service_name', 'asc')->pluck('service_name', 'service_id');
         $jdir = 'public';///images/'.$job->job_id;
-        $files = $storagePath  = Storage::files($jdir);
+        $files  = Storage::files($jdir);
         $directories = Storage::allDirectories($jdir);
         //edit view
         return view('jobs.edit')
