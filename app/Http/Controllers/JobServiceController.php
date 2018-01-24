80c84dd34db2bdfeaa831adb374808009543ba48
Chad Quilter <chadquilter@gmail.com>
2018-01-23 19:46:48 -0600

    add new job services table, with controller and factory

---
+++ app/Http/Controllers/JobServiceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\JobService;
+use Illuminate\Http\Request;
+
+class JobServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\JobService  $jobService
+     * @return \Illuminate\Http\Response
+     */
+    public function show(JobService $jobService)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\JobService  $jobService
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(JobService $jobService)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\JobService  $jobService
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, JobService $jobService)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\JobService  $jobService
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(JobService $jobService)
+    {
+        //
+    }
+}
