77e51ce1e2e0fa31e15a667ce18c90a81c0197bf
Chad Quilter <chadquilter@hotmail.com>
2018-01-24 19:52:39 -0600

    add new image blade to job index file

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -19,7 +19,7 @@
 <body>
   <main role="main">
     @include('inc.navbar')
-    <div class="body-container" style="background-attachment:fixed;background-color:#fff;background-position:center top;background-repeat:repeat-x;background-image: url(/images/art-bg.jpg); width=100%;">
+    <div class="body-container" style="background-attachment:fixed;background-color:#fff;background-position:center top;background-repeat:repeat-x;background-image: url(/images/art-bg.jpg); width=100%; z-index: 1;">
       <br>
       @include('inc.messages')
       <br>
