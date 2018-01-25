79d9ffb97e63c192c4f129f589c643a780eb4206
Chad Quilter <chadquilter@gmail.com>
2018-01-24 22:16:27 -0600

    add custom scss

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,7 +7,7 @@
     <br>
     <div class="card image_display_r">
       <div class="card-img-top">
-        <div id="carouselExampleIndicators" class="carousel carousel-fade" data-ride="carousel">
+        <div id="carouselExampleIndicators" class="carousel" data-ride="carousel">
           <ol class="carousel-indicators">
             <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
