ba2caa0a7464f0d1d743591ee80cf1e142fff966
Chad Quilter <chadquilter@hotmail.com>
2018-01-25 15:24:14 -0600

    checkpoint --- carousel css where it needs to be

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -11,65 +11,67 @@
           @endphp
           <br>
           @include('inc.pagelabel')
-          <div class="jumbotron" class="border border-primary rounded">
-            <div class="row">
-              <div class="col">
-                <center>
-                  {{$jobs->links()}}
-                </center>
-              </div>
-            </div>
-
-            <div class="card">
-              <div class="card-body">
-                <p class="lead"> <strong class="display-4">Services Provided:</strong></p>
-                {!! Form::open() !!}
-                <div id="job_service_group" class="form=group">
-                  <div class="alert alert-info" role="alert">
-                    <div class="row">
-                      @if(count($mdg_services) > 0)
-
-                        @foreach($mdg_services as $mdg_id => $mdg_name)
-                          @php
-                          $has_service = App\Job::find($job->job_id)->services()->where('service_id', $mdg_id)->first();
-                          $job_service_checked = count($has_service) > 0 ? 'true' : '';
-                          @endphp
-                          @if ($job_service_checked != '')
-                            <div class="col-4 mx-auto">
-                              <div class="card image_display_r border border-secondary rounded shadow_only">
-                                <div class="card-title">
-                                  {{Form::label('serviceID[]', $mdg_name)}} {{Form::checkbox('serviceID[]', '', $job_service_checked, ['class' => 'form-control', 'disabled' => 'true' ])}}
-                                </div>
-                              </div>
-                            </div>
-                          @endif
-                        @endforeach
-
-                      @else
-                        <h1>No Types Listed!</h1>
-                      @endif
-                    </div>
-                  </div>
+          <div class="card" class="border border-primary rounded">
+            <div class="card-body">
+              <div class="row">
+                <div class="col">
+                  <center>
+                    {{$jobs->links()}}
+                  </center>
                 </div>
-                {!! Form::close() !!}
               </div>
+
               <div class="card">
                 <div class="card-body">
-                  <p class="lead">{!! $job->job_summary !!}</p>
+                  <p class="lead"> <strong class="display-4">Services Provided:</strong></p>
+                  {!! Form::open() !!}
+                  <div id="job_service_group" class="form=group">
+                    <div class="alert alert-info" role="alert">
+                      <div class="row">
+                        @if(count($mdg_services) > 0)
+
+                          @foreach($mdg_services as $mdg_id => $mdg_name)
+                            @php
+                            $has_service = App\Job::find($job->job_id)->services()->where('service_id', $mdg_id)->first();
+                            $job_service_checked = count($has_service) > 0 ? 'true' : '';
+                            @endphp
+                            @if ($job_service_checked != '')
+                              <div class="col-4 mx-auto">
+                                <div class="card image_display_r border border-secondary rounded shadow_only">
+                                  <div class="card-title">
+                                    {{Form::label('serviceID[]', $mdg_name)}} {{Form::checkbox('serviceID[]', '', $job_service_checked, ['class' => 'form-control', 'disabled' => 'true' ])}}
+                                  </div>
+                                </div>
+                              </div>
+                            @endif
+                          @endforeach
+
+                        @else
+                          <h1>No Types Listed!</h1>
+                        @endif
+                      </div>
+                    </div>
+                  </div>
+                  {!! Form::close() !!}
+                </div>
+                <div class="card">
+                  <div class="card-body">
+                    <p class="lead">{!! $job->job_summary !!}</p>
+                  </div>
+                </div>
+                @php
+                $jdir = 'job'.$job->job_id.'/';
+                $files = Storage::disk('images')->files($jdir);
+                @endphp
+                <div class="card image_display_r">
+                  <div class="card-img-top">
+                    @include('inc.jobimagecarosel')
+                  </div>
                 </div>
               </div>
-              @php
-              $jdir = 'job'.$job->job_id.'/';
-              $files = Storage::disk('images')->files($jdir);
-              @endphp
-              <div class="card image_display_r">
-                <div class="card-img-top">
-                  @include('inc.jobimagecarosel')
-                </div>
-              </div>
+              <br>
+              {{$jobs->links()}}
             </div>
-            <br>
-            {{$jobs->links()}}
           </div>
         </div>
       @endforeach
