f335c2e7afb36792d4ad8b6482ffa6d1117ba241
Chad Quilter <chadquilter@gmail.com>
2018-01-26 15:27:42 -0600

    update page label blade file

--- resources/views/inc/pagelabel.blade.php
+++ resources/views/inc/pagelabel.blade.php
@@ -1,15 +1,13 @@
 <div class="card border border-secondary">
   <div class="card-body">
-    <div class="col">
       <div class="row">
-        <div class="col-sm-4 alert alert-primary">
+        <div class="col alert alert-primary">
           <h4 class="display-4"><span class="badge badge-secondary">BAM!</span></h4>
         </div>
         <div class="col alert alert-light image_display_r">
           <h4 class="display-4"> {{ $title }} </h4>
         </div>
       </div>
-    </div>
   </div>
 </div>
 <br>
