b23783967fe4164cc15a8a9a6c60e5586f05558b
Chad Quilter <chadquilter@gmail.com>
2018-01-26 14:24:03 -0600

    footer layout

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,13 +1,17 @@
 <footer class="footer">
-  <div class="foot-container navbar-dark bg-dark fixed-bottom">
+  <div class="foot-container navbar-dark bg-dark">
     <div class="footer-top-1">
       &nbsp;
     </div>
     <div class="footer-text" styel="width:70%;">
       <center>
         <p class="text-muted">
-          ‡ Serving ‡
-          <br>
+          ‡ Contact Us:‡<br>
+          P.O. Box 1583<br>
+          Marble Falls Tx<br>
+          78654<br>
+          Tele: (830)220-2876 (830)265-0941<br>
+          ‡ Serving ‡<br>
           Marble Falls, TX • Austin, TX • Houston, TX • San Antonio, TX • Dallas/Fort Worth, TX • Corpus Christi, TX • El Paso, TX • Texas, USA.
           <br>
           2018,  {{ config('app.name', 'MDG') }}
@@ -18,10 +22,6 @@
       <center>
         <img class="footer-badge" height="35" width="35" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Energy_Star_logo.svg/586px-Energy_Star_logo.svg.png">
       </center>
-      <br>
-    </div>
-    <div class="footer-bottom">
-      &nbsp;
     </div>
   </div>
 </footer>
