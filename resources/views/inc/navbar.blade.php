88c16aa6c418a65f8d9e1990370d814a7f157ed9
Chad Quilter <chadquilter@gmail.com>
2018-01-26 15:17:23 -0600

    add contact to nav bar with phone icon

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -37,7 +37,7 @@
       </li>
     </ul>
     <div class="text-small text-white">
-      <img src="/images/icons/phoneicon.jpeg" width="30" height="30" class="d-inline-block align-top" alt="phone-icon">
+      <img src="/images/icons/phoneicon.png" width="30" height="30" class="d-inline-block align-top" alt="phone-icon">
       Phone: (830)220-2876 •---• (830)265-0941
     </div>
     <ul class="navbar-nav ml-auto">
