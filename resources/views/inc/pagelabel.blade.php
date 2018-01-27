fd56b10d79d18b1eab802c4f8d61c5c0bc8cf51e
Chad Quilter <chadquilter@gmail.com>
2018-01-26 19:36:48 -0600

    update index, jumbotrons having trouble containing content in older browsers

--- resources/views/inc/pagelabel.blade.php
+++ resources/views/inc/pagelabel.blade.php
@@ -2,7 +2,7 @@
   <div class="card-body">
       <div class="row">
         <div class="col-sm-3 alert alert-primary">
-          <h4 class="display-4"><span class="badge badge-secondary">BAM!</span></h4>
+          <h4 class="display-4"><div class="badge badge-secondary">BAM!</div></h4>
         </div>
         <div class="col-sm-6 alert alert-light">
           <h4 class="display-4"> {{ $title }} </h4>
