b936259c8a453df09365c1d3375bbbad5a5e19cc
Chad Quilter <chadquilter@gmail.com>
2018-01-26 20:49:03 -0600

    use lead tag for contacts blade

--- resources/views/quotes/create.blade.php
+++ resources/views/quotes/create.blade.php
@@ -7,7 +7,7 @@
     @include('inc.pagelabel')
     <br>
     <div class="card bg-secondary border border-dark rounded image_display_r">
-      <div class="card-body">
+      <div class="card-body row">
         <div class="col">
           <div class="card border border-dark rounded image_display_r">
             <img class="card-img-top" src="/images/mdg_images/IMG_BATHROOM2.JPG" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
