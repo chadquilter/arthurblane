8076c005a1a18a044cb9afb922d49cb87d28e59e
Chad Quilter <chadquilter@gmail.com>
2018-01-26 19:57:47 -0600

    add image to page label and new css

--- resources/views/inc/pagelabel.blade.php
+++ resources/views/inc/pagelabel.blade.php
@@ -2,8 +2,9 @@
   <div class="card-body">
       <div class="row">
         <div class="col alert alert-primary">
-
-          <img src="/images/icons/hammernail.jpg">
+          <div class="card image_display_r">
+            <img class="card-img-top" src="/images/icons/hammernail.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
+          </div>
         </div>
         <div class="col alert alert-light">
           <h4 class="display-4"> {{ $title }} </h4>
