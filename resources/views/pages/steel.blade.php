59535d36975b154f5123b008c3c5998055b771bd
Chad Quilter <chadquilter@hotmail.com>
2018-02-07 20:38:35 -0600

    update card class to dark style

--- resources/views/pages/steel.blade.php
+++ resources/views/pages/steel.blade.php
@@ -68,7 +68,7 @@
 
 		<br>
 
-		<div class="card image_display_r">
+		<div class="card border border-dark rounded image_display_r ">
 			<img class="card-img-top" alt="A scenic home image" src="/images/steel/steel_barn1.jpg" alt="Custom Houses, Custom Construction, custom tile, carpet, drywall, Custom Metal Work, Paving, Asphalt, Show House, kitchen, bath House Remodeling, House Renovations, House Repair, Custom Deck, tile, shower,Custom Bridges, Custom Roads, Excivation, Interior Design, Custom Patio, rock floor">
 			<div class="card-body">
 				<div class="card-title">
