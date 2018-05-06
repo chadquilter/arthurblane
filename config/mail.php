02f39823c44f4b67b724a0f9a1b9ab7ff8b0ddc8
Chad Quilter <chadquilter@hotmail.com>
2018-05-06 18:19:41 -0500

    update laravel to use mailgun instead of sendmail

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.bamconstruction.net'),
+    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
 
     /*
     |--------------------------------------------------------------------------
@@ -53,11 +53,11 @@ return [
     | the same address. Here, you may specify a name and address that is
     | used globally for all e-mails that are sent by your application.
     |
-
+    */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'SUPER_ADMIN'),
-        'name' => env('MAIL_FROM_NAME', 'Bam! Construction'),
+        'address' => env('NOREPLY_EMAIL', ''),
+        'name' => env('NOREPLY_NAME', ''),
     ],
 
     /*
