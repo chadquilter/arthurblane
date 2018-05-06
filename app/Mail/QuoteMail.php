116b1992530d19ed19cbd4140fa44b88363034c4
Chad Quilter <chadquilter@hotmail.com>
2018-05-06 18:21:37 -0500

    update laravel to use mailgun instead of sendmail

--- app/Mail/QuoteMail.php
+++ app/Mail/QuoteMail.php
@@ -25,8 +25,8 @@ class QuoteMail extends Mailable
         $this->quote = $quote;
         $to = explode(',', env('ADMIN_EMAILS'));
         $this->to($to);
-        $this->from('mikegrounds@bamconstruction.net', 'Mike Grounds');
-        $this->replyto('mikegrounds@bamconstruction.net', 'Mike Grounds');
+        //$this->from('mikegrounds@bamconstruction.net', 'Mike Grounds');
+        //$this->replyto('mikegrounds@bamconstruction.net', 'Mike Grounds');
         $this->subject('Hello '.$this->quote->title.' has contacted Bam Construction!');
 
     }
