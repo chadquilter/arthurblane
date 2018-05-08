3eb9e5970ffd7c3aaed0f9353f359da509e772e5
Chad Quilter <chadquilter@gmail.com>
2018-05-08 17:13:56 -0500

    add all the new address changes and updates

--- app/Form.php
+++ app/Form.php
@@ -31,7 +31,7 @@ class Form extends Model
     }
 
     public function addresses(){
-        return $this->belongsToMany('App\FormAddress', 'form_adresses', 'form_address_address_id', 'address_id');
+        return $this->belongsToMany('App\FormAddress', 'form_addresses', 'form_address_address_id', 'address_id');
     }
 
     public function user(){
