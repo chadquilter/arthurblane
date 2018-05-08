84a181caf4b159bc538f95171cd1640c25a63c87
Chad Quilter <chadquilter@hotmail.com>
2018-05-08 01:37:44 -0500

    add first stage of PDF templating

--- resources/views/pdf/pdf1.blade.php
+++ resources/views/pdf/pdf1.blade.php
@@ -59,12 +59,12 @@
 
           <table class="table" id="dynamic_field">
             <tbody>
-              @if(count($form_items_records) > 0)
+              @if(count($form->items()) > 0)
                 @foreach($form_items_records as $formItem)
                   <tr id="row{{ $loop->iteration }}">
                     <td id="item_div_{{$loop->iteration}}">
                       <strong>Item:</strong>
-                      {{ $formItem->items_name }}
+                      {{ $formItem->items_id }}
                     </td>
                     <td>
                       <strong>Amount: </strong>
