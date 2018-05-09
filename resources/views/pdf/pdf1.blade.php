ac895743f2f4ff5d68abd9b382f7bdbd14cebeb9
Chad Quilter <chadquilter@gmail.com>
2018-05-09 04:04:26 -0500

    add all the new address changes and updates

--- resources/views/pdf/pdf1.blade.php
+++ resources/views/pdf/pdf1.blade.php
@@ -64,7 +64,7 @@
                   <tr id="row{{ $loop->iteration }}">
                     <td id="item_div_{{$loop->iteration}}">
                       <strong>Item:</strong>
-                      {{ $formItem->item->name }}
+                      {{ $formItem->item }}
                     </td>
                     <td>
                       <strong>Amount: </strong>
