7e8a57dff04b0427e35195adceee44d938fe3f04
Chad Quilter <chadquilter@gmail.com>
2018-05-11 02:52:20 -0500

    commit update to table

--- app/Form.php
+++ app/Form.php
@@ -33,7 +33,7 @@ class Form extends Model
 
     public function addresses()
     {
-      return $this->belongsToMany('App\Address', 'form_addresses', 'form_id', 'form_id');
+      return $this->belongsToMany('App\Address', 'form_addresses', 'id', 'form_id');
     }
 
     public function user()
